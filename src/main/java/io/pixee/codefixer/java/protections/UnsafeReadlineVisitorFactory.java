81c6b4900e0b52ed37a17e422cf187c43e6f80d1
Arshan Dabirsiaghi <arshan@pixee.ai>
2022-05-03 16:14:49 -0400

    readline ng

--- src/main/java/io/pixee/codefixer/java/protections/UnsafeReadlineVisitorFactory.java
+++ src/main/java/io/pixee/codefixer/java/protections/UnsafeReadlineVisitorFactory.java
@@ -5,6 +5,9 @@ import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.expr.*;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
 import com.github.javaparser.ast.visitor.Visitable;
+import io.pixee.codefixer.java.MethodCallTransformingModifierVisitor;
+import io.pixee.codefixer.java.NodePredicateFactory;
+import io.pixee.codefixer.java.Transformer;
 import io.pixee.security.*;
 import io.pixee.codefixer.java.FileWeavingContext;
 import io.pixee.codefixer.java.TypeLocator;
@@ -13,50 +16,43 @@ import io.pixee.codefixer.java.Weave;
 
 import java.io.File;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Predicate;
 
 public final class UnsafeReadlineVisitorFactory implements VisitorFactory {
 
+
   @Override
-  public ModifierVisitor<FileWeavingContext> createJavaCodeVisitorFor(
-      final File file, CompilationUnit cu) {
-    return new UnsafeReadlineVisitor(cu);
+  public ModifierVisitor<FileWeavingContext> createJavaCodeVisitorFor(final File file, final CompilationUnit cu) {
+    Set<Predicate<MethodCallExpr>> predicates = Set.of(
+            NodePredicateFactory.withMethodName("readLine"),
+            NodePredicateFactory.withArgumentCount(0),
+            NodePredicateFactory.withScopeType(cu, "java.io.BufferedReader"),
+            NodePredicateFactory.withScreamingSnakeCaseVariableNameForArgument(1).negate()
+    );
+
+    Transformer<MethodCallExpr> transformer = new Transformer<>() {
+      @Override
+      public TransformationResult<MethodCallExpr> transform(final MethodCallExpr methodCallExpr, final FileWeavingContext context) {
+        Expression readerScope = methodCallExpr.getScope().get();
+        MethodCallExpr safeExpression =
+                new MethodCallExpr(new NameExpr(SafeIO.class.getName()), "boundedReadLine");
+        safeExpression.setArguments(
+                NodeList.nodeList(readerScope, new IntegerLiteralExpr(defaultLineMaximum)));
+        Weave weave = Weave.from(methodCallExpr.getRange().get().begin.line, readlineRuleId);
+        methodCallExpr.getParentNode().get().replace(methodCallExpr, safeExpression);
+        return new TransformationResult<>(Optional.of(safeExpression), weave);
+      }
+    };
+
+    return new MethodCallTransformingModifierVisitor(cu, predicates, transformer);
   }
 
     @Override
     public String ruleId() {
         return readlineRuleId;
     }
-
-    private static class UnsafeReadlineVisitor extends ModifierVisitor<FileWeavingContext> {
-    private final CompilationUnit cu;
-
-    private UnsafeReadlineVisitor(final CompilationUnit cu) {
-      this.cu = Objects.requireNonNull(cu);
-    }
-
-    @Override
-    public Visitable visit(MethodCallExpr n, final FileWeavingContext context) {
-      if ("readLine".equals(n.getNameAsString())
-          && n.getScope().isPresent()
-          && context.isLineIncluded(n)) {
-        NodeList<Expression> arguments = n.getArguments();
-        if (arguments.size() == 0) {
-          Expression readerScope = n.getScope().get();
-          TypeLocator resolver = TypeLocator.createDefault(cu);
-          if ("java.io.BufferedReader".equals(resolver.locateType(readerScope))) {
-            MethodCallExpr safeExpression =
-                new MethodCallExpr(new NameExpr(SafeIO.class.getName()), "boundedReadLine");
-            safeExpression.setArguments(
-                NodeList.nodeList(readerScope, new IntegerLiteralExpr(defaultLineMaximum)));
-            context.addWeave(Weave.from(n.getRange().get().begin.line, readlineRuleId));
-            n.getParentNode().get().replace(n, safeExpression);
-            n = safeExpression;
-          }
-        }
-      }
-      return super.visit(n, context);
-    }
-  }
 
   private static final int defaultLineMaximum = 1_000_000; // 1 MB
   private static final String readlineRuleId = "pixee:java/readline";
