52b726170a02a042bcfcc2f52685f8ebe9ea4bd9
idolieb26 <idolieb26@gmail.com>
2022-09-08 15:36:16 -0400

    Create a simple DSL parser

---
+++ src/main/java/io/pixee/codetl/java/JavaDSLListener.java
@@ -0,0 +1,38 @@
+package io.pixee.codetl.java;
+
+import io.pixee.dsl.java.DSLParser;
+import io.pixee.dsl.java.DSLBaseListener;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
+public class JavaDSLListener extends DSLBaseListener {
+    @Override
+    public void enterRule_id(DSLParser.Rule_idContext ctx) {
+        System.out.println("enterRule_id::" + ctx.id.getText());
+    }
+    
+    @Override
+    public void exitRule_id(DSLParser.Rule_idContext ctx) {
+    	System.out.println("exitRule_id::" + ctx.id.getText());
+    }
+    
+    @Override
+    public void enterMatch(DSLParser.MatchContext ctx) {
+    	System.out.println("enterMatch::" + ctx.getText());
+    }
+	
+	@Override
+	public void exitMatch(DSLParser.MatchContext ctx) {
+		System.out.println("exitMatch::" + ctx.getText());
+	}
+	
+	@Override public void enterReplace(DSLParser.ReplaceContext ctx) {
+		var text = ctx.getText().split("with")[1].strip();
+		System.out.println("enterReplace::" + text);
+	}
+}
