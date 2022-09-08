6023d0afbeeca8cba3b5eaabe57ab812ce39b246
idolieb26 <idolieb26@gmail.com>
2022-09-08 15:37:48 -0400

    Add test dsl file

---
+++ src/test/java/io/pixee/codetl/java/TestDSL.java
@@ -0,0 +1,39 @@
+package io.pixee.codetl.java;
+
+import org.junit.jupiter.api.Test;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import io.pixee.dsl.java.DSLLexer;
+import io.pixee.dsl.java.DSLParser;
+import io.pixee.codetl.java.JavaDSLListener;
+
+
+public class TestDSL {
+    @Test
+    void get_parser() throws Exception {
+        var input = "rule pixee:java/secure-random\n" +
+                "match\n" +
+                "\tConstructorCall {\n" +
+                "\ttarget = Random\n" +
+                "\t}\n" +
+                "replace $c with\n" +
+                "\tConstructorCall {\n" +
+                "\ttarget = java.security.SecureRandom\n" +
+                "\t}\n";
+
+        var chars = CharStreams.fromString(input);
+        var lexer = new DSLLexer(chars);
+        var tokens = new CommonTokenStream(lexer);
+
+        var parser = new DSLParser(tokens);
+        ParseTree tree = parser.start();
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+        JavaDSLListener listener = new JavaDSLListener();
+        walker.walk(listener, tree);
+    }
+}
