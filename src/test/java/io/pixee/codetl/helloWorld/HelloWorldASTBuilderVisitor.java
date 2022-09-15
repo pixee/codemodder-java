b544279282781c089889380ab693409adf258811
Markus Voelter <voelter@acm.org>
2022-09-15 14:37:07 +0200

    cleanup

---
+++ src/test/java/io/pixee/codetl/helloWorld/HelloWorldASTBuilderVisitor.java
@@ -0,0 +1,36 @@
+package io.pixee.codetl.helloWorld;
+
+import io.pixee.ast.CodeUnit;
+import io.pixee.ast.Node;
+import io.pixee.ast.Value;
+import io.pixee.codetl.helloWorld.grammar.helloWorldGrammarBaseVisitor;
+import io.pixee.codetl.helloWorld.grammar.helloWorldGrammarParser;
+import io.pixee.meta.PrimitiveType;
+
+public class HelloWorldASTBuilderVisitor extends helloWorldGrammarBaseVisitor<Node> {
+
+    private CodeUnit unit;
+    private HelloWorldLanguage hw;
+
+    public CodeUnit getCode() {
+        return unit.resolve();
+    }
+
+    @Override public Node visitProgram(helloWorldGrammarParser.ProgramContext ctx) {
+        hw = new HelloWorldLanguage();
+        Node program = new Node(hw.PROGRAM);
+        unit = new CodeUnit(hw.LANG, program);
+        for (helloWorldGrammarParser.VariableContext c: ctx.variable()) {
+            program.add("variables", createVariable(c));
+        }
+        return visitChildren(ctx);
+    }
+
+    private Node createVariable(helloWorldGrammarParser.VariableContext ctx) {
+        Node var = new Node(hw.VAR);
+        var.add("name", new Value(PrimitiveType.STRING, ctx.ID().getText()));
+        var.add("initial", new Node(hw.NUM_LIT).add("value", new Value(PrimitiveType.STRING, ctx.numlit().NUMBER().getText())));
+        return var;
+    }
+
+}
--- src/test/java/io/pixee/codetl/helloWorld/HelloWorldLanguage.java
+++ src/test/java/io/pixee/codetl/helloWorld/HelloWorldLanguage.java
@@ -4,16 +4,16 @@ import io.pixee.meta.*;
 
 public class HelloWorldLanguage {
 
-    public final LanguageDescriptor DESCRIPTOR;
+    public final LanguageDescriptor LANG;
     public final ConceptDescriptor NUM_LIT;
     public final ConceptDescriptor VAR;
     public final ConceptDescriptor PROGRAM;
 
     public HelloWorldLanguage() {
-        DESCRIPTOR = new LanguageDescriptor("HelloWorld");
-        NUM_LIT = DESCRIPTOR.newConcept("NumLit").add(new PropertyDescriptor("value", PrimitiveType.STRING));
-        VAR = DESCRIPTOR.newConcept("Var").add(new PropertyDescriptor("initial", NUM_LIT.getType())).add(new PropertyDescriptor("name", PrimitiveType.STRING));
-        PROGRAM = DESCRIPTOR.newConcept("Program").add(new PropertyDescriptor("variables", VAR.getType(),true));
+        LANG = new LanguageDescriptor("HelloWorld");
+        NUM_LIT = LANG.newConcept("NumLit").add(new PropertyDescriptor("value", PrimitiveType.STRING));
+        VAR = LANG.newConcept("Var").add(new PropertyDescriptor("initial", NUM_LIT.getType())).add(new PropertyDescriptor("name", PrimitiveType.STRING));
+        PROGRAM = LANG.newConcept("Program").add(new PropertyDescriptor("variables", VAR.getType(),true));
     }
 
 }
