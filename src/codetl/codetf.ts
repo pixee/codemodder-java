084a43f5168f812090c8a641948238fa91829ee0
Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>
2022-09-24 10:20:59 -0400

    codetf interfaces added

--- src/codetl/codetf.ts
+++ src/codetl/codetf.ts
@@ -3,7 +3,49 @@
  * @see https://github.com/openpixee/codetf
  */
 export interface CodeTF {
-    foo: string
+    run: CodeTFRun
+    results: CodeTFResult[]
+}
+
+export interface CodeTFRun {
+    vendor: string,
+    tool: string,
+    commandLine: string,
+    elapsed: number,
+    configuration: CodeTFConfiguration,
+    fileExtensionsScanned: CodeTFFileExtensionScanned[]
+}
+
+export interface CodeTFFileExtensionScanned {
+    extension: string,
+    count: number
+}
+
+export interface CodeTFResult {
+    path: string,
+    diff: string,
+    changes: CodeTFChange[]
+}
+
+export interface CodeTFChange {
+    lineNumber: number,
+    category: string,
+    description: string,
+    properties: Map<any,any>
+}
+
+export interface CodeTFConfiguration {
+    directory: string,
+    inputs: CodeTFInput,
+    modules: string[],
+    includes: string[],
+    excludes: string[]
+}
+
+export interface CodeTFInput {
+    artifact: string,
+    sha1: string,
+    vendor: string
 }
 
 /**
--- test/args.test.ts
+++ test/args.test.ts
@@ -1,4 +1,4 @@
-import {expect, jest, test} from '@jest/globals';
+import {expect, test} from '@jest/globals';
 import {ArgumentParser} from '../src/codetl/args';
 
 const parser = new ArgumentParser();
