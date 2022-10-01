e0f0c9ee3d803cc55ce6bad45b22501e9dee1dac
Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>
2022-10-01 18:15:29 -0400

    wip

--- src/codetl/langs/java.ts
+++ src/codetl/langs/java.ts
@@ -1,9 +1,47 @@
 import {LanguageProvider} from "../providers";
 import {CodeTLExecutionContext} from "../cli";
+import tempfile from 'tempfile';
 
+/**
+ * This module supports analyzing/transforming Java code
+ * @see https://github.com/openpixee/java-code-hardener
+ */
 export class JavaLanguageProvider implements LanguageProvider {
     process(codetlExecutionContext: CodeTLExecutionContext): string {
-        return "";
-    }
+        // @ts-ignore
+        let javaHardenerEntryType = Java.type('io.pixee.codefixer.java.JavaFixitCli');
+        const outputFile = tempfile(".codetf");
+        const args : string[] = []
+        args.push('--output=' + outputFile)
+        args.push('--repository=' + codetlExecutionContext.repository)
+        args.push('--rule-default='+ codetlExecutionContext.ruleDefault)
 
+        if(codetlExecutionContext.ruleExceptions != undefined) {
+            codetlExecutionContext.ruleExceptions.forEach((exc) => {
+                args.push('--rule-exception=' + exc)
+            });
+        }
+        if(codetlExecutionContext.includes != undefined) {
+            codetlExecutionContext.includes.forEach((include) => {
+                args.push('--include=' + include)
+            });
+        }
+
+        if(codetlExecutionContext.excludes != undefined) {
+            codetlExecutionContext.excludes.forEach((exclude) => {
+                args.push('--exclude=' + exclude)
+            });
+        }
+
+        if(codetlExecutionContext.sarifFilePaths != undefined) {
+            codetlExecutionContext.sarifFilePaths.forEach((sarifFilePath) => {
+                args.push('--input=' + sarifFilePath)
+            });
+        }
+
+        javaHardenerEntryType.main(args)
+
+        // the java module will have populated the output file
+        return outputFile;
+    }
 }
