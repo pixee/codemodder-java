5ddf298aa8e3c5882aef7f1ca45da1148a547984
andrecs <12188364+andrecsilva@users.noreply.github.com>
2023-02-08 15:19:31 -0300

    Upgraded javaparser, fixed and enabled test

--- languages/java/src/test/java/com/acme/testcode/resourceleak/JDBCSimpleLeak.java
+++ languages/java/src/test/java/com/acme/testcode/resourceleak/JDBCSimpleLeak.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class JDBCSimpleLeak {
+public final class JDBCSimpleLeak {
 
   private Connection conn;
 
--- languages/java/src/test/java/com/acme/testcode/resourceleak/JDBCSimpleLeakWeaved.java
+++ languages/java/src/test/java/com/acme/testcode/resourceleak/JDBCSimpleLeakWeaved.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class JDBCSimpleLeakWeaved {
+public final class JDBCSimpleLeakWeaved {
 
   private Connection conn;
 
