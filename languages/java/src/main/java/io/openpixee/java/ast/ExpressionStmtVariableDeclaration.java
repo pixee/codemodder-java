9a35d6572a903895ffac94499942be32880bbb86
andrecs <12188364+andrecsilva@users.noreply.github.com>
2023-02-14 08:00:48 -0300

    Better documentation and member visibility

--- languages/java/src/main/java/io/openpixee/java/ast/ExpressionStmtVariableDeclaration.java
+++ languages/java/src/main/java/io/openpixee/java/ast/ExpressionStmtVariableDeclaration.java
@@ -3,26 +3,33 @@ package io.openpixee.java.ast;
 import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 import com.github.javaparser.ast.stmt.ExpressionStmt;
+import java.util.Objects;
 
+/**
+ * Holds the nodes in the AST that represents a local variable declaration statement of a variable.
+ * See <a href="https://docs.oracle.com/javase/specs/jls/se19/html/jls-14.html#jls-14.4.2">Java
+ * Language Specification - Section 14.4.2</a> for more details.
+ */
 public final class ExpressionStmtVariableDeclaration extends LocalVariableDeclaration {
 
   private final ExpressionStmt stmt;
 
   public ExpressionStmtVariableDeclaration(
-      ExpressionStmt stmt, VariableDeclarationExpr vde, VariableDeclarator vd) {
-    this.stmt = stmt;
-    this.vde = vde;
-    this.vd = vd;
+      final ExpressionStmt stmt, final VariableDeclarationExpr vde, final VariableDeclarator vd) {
+    this.stmt = Objects.requireNonNull(stmt);
+    this.vde = Objects.requireNonNull(vde);
+    this.vd = Objects.requireNonNull(vd);
     this.scope = null;
   }
 
+  /** Returns the {@link ExpressionStmt} {@link Node} that holds this local declaration. */
   @Override
   public ExpressionStmt getStatement() {
     return stmt;
   }
 
   @Override
-  public LocalVariableScope findScope() {
+  protected LocalVariableScope findScope() {
     return LocalVariableScope.fromLocalDeclaration(stmt, vd);
   }
 }
--- languages/java/src/main/java/io/openpixee/java/ast/LocalVariableDeclaration.java
+++ languages/java/src/main/java/io/openpixee/java/ast/LocalVariableDeclaration.java
@@ -7,31 +7,36 @@ import com.github.javaparser.ast.stmt.Statement;
 /**
  * Holds the nodes in the AST that represents a local declaration of a variable. See <a
  * href="https://docs.oracle.com/javase/specs/jls/se19/html/jls-6.html#jls-6.1">Java Language
- * Specification - Section 6.1</a>
+ * Specification - Section 6.1</a> for all the possible ways a local variable can be declarared.
  */
 public abstract class LocalVariableDeclaration {
 
-  VariableDeclarationExpr vde;
-  VariableDeclarator vd;
-  LocalVariableScope scope;
+  protected VariableDeclarationExpr vde;
+  protected VariableDeclarator vd;
+  protected LocalVariableScope scope;
 
+  /** Returns the name of the local variable in this declaration as a {@link String}. */
   public String getName() {
     return vd.getNameAsString();
   }
 
+  /** Returns the {@link LocalVariableScope} of the local variable in this declaration. */
   public LocalVariableScope getScope() {
     if (scope == null) scope = findScope();
     return scope;
   }
 
+  /** Returns the {@link VariableDeclarator} {@link Node} that holds this local declaration. */
   public VariableDeclarator getVariableDeclarator() {
     return vd;
   }
 
+  /** Returns the {@link VariableDeclarationExpr} {@link Node} that holds this local declaration. */
   public VariableDeclarationExpr getVariableDeclarationExpr() {
     return vde;
   }
 
+  /** Returns the {@link Statement} {@link Node} that holds this local declaration. */
   public abstract Statement getStatement();
 
   protected abstract LocalVariableScope findScope();
