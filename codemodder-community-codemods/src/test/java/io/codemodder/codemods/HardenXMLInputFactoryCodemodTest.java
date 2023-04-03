32eebf28124593f0241f730c84758abb47a98b33
Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>
2023-04-03 19:18:14 -0400

    works

---
+++ codemodder-community-codemods/src/test/java/io/codemodder/codemods/HardenXMLInputFactoryCodemodTest.java
@@ -0,0 +1,10 @@
+package io.codemodder.codemods;
+
+import io.codemodder.testutils.CodemodTestMixin;
+import io.codemodder.testutils.Metadata;
+
+@Metadata(
+    codemodType = HardenXMLInputFactoryCodemod.class,
+    testResourceDir = "harden-xmlinputfactory",
+    dependencies = "io.github.pixee:java-security-toolkit:1.0.1")
+final class HardenXMLInputFactoryCodemodTest implements CodemodTestMixin {}
