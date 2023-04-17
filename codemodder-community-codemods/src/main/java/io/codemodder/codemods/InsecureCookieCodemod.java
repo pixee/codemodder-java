ebc401631b35aec9e559bb4869967ae582297b69
andrecs <12188364+andrecsilva@users.noreply.github.com>
2023-04-17 11:15:21 -0300

    Converted InsecureCookieVisitor to Codemod

---
+++ codemodder-community-codemods/src/main/java/io/codemodder/codemods/InsecureCookieCodemod.java
@@ -0,0 +1,66 @@
+package io.codemodder.codemods;
+
+import com.contrastsecurity.sarif.Result;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.expr.BooleanLiteralExpr;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.stmt.ExpressionStmt;
+import com.github.javaparser.ast.stmt.Statement;
+import io.codemodder.Codemod;
+import io.codemodder.CodemodInvocationContext;
+import io.codemodder.RegionExtractor;
+import io.codemodder.ReviewGuidance;
+import io.codemodder.RuleSarif;
+import io.codemodder.SarifPluginJavaParserChanger;
+import io.codemodder.ast.ASTTransforms;
+import io.codemodder.providers.sarif.codeql.CodeQLScan;
+import java.util.Optional;
+import javax.inject.Inject;
+
+/** Fixes issues reported under the id "java/insecure-cookie". */
+@Codemod(
+    id = "codeql:java/insecure-cookie",
+    author = "andre.silva@pixee.ai",
+    reviewGuidance = ReviewGuidance.MERGE_WITHOUT_REVIEW)
+public class InsecureCookieCodemod extends SarifPluginJavaParserChanger<MethodCallExpr> {
+
+  @Inject
+  public InsecureCookieCodemod(@CodeQLScan(ruleId = "java/insecure-cookie") final RuleSarif sarif) {
+    super(sarif, MethodCallExpr.class, RegionExtractor.FROM_FIRST_LOCATION);
+  }
+
+  @Override
+  public boolean onResultFound(
+      CodemodInvocationContext context,
+      CompilationUnit cu,
+      MethodCallExpr methodCallExpr,
+      Result result) {
+    // This assumption is a bit strong, but covers the most common cases while avoiding weird ones
+    var maybeStmt =
+        methodCallExpr
+            .getParentNode()
+            .map(p -> p instanceof Statement ? (Statement) p : null)
+            .filter(stmt -> stmt.isExpressionStmt());
+
+    // We want to avoid things like: response.addCookie(new Cookie(...)), so we restrict ourselves
+    Optional<Expression> maybeCookieExpression =
+        Optional.of(methodCallExpr.getArgument(0))
+            .filter(expr -> expr.isNameExpr() || expr.isFieldAccessExpr());
+
+    if (maybeStmt.isPresent() && maybeCookieExpression.isPresent()) {
+      var newStatement =
+          new ExpressionStmt(
+              new MethodCallExpr(
+                  maybeCookieExpression.get(),
+                  "setSecure",
+                  new NodeList<>(new BooleanLiteralExpr(true))));
+
+      ASTTransforms.addStatementBeforeStatement(maybeStmt.get(), newStatement);
+      // Should we add a setSecure(false) after to retain behavior?
+      return true;
+    }
+    return false;
+  }
+}
