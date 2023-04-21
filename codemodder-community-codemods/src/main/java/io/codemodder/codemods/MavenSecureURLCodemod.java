0e99c0b73a9dd5be114013af616b8bf9c594e27b
andrecs <12188364+andrecsilva@users.noreply.github.com>
2023-04-21 10:32:11 -0300

    Actual implementation and more tests

--- codemodder-community-codemods/src/main/java/io/codemodder/codemods/MavenSecureURLCodemod.java
+++ codemodder-community-codemods/src/main/java/io/codemodder/codemods/MavenSecureURLCodemod.java
@@ -1,10 +1,30 @@
 package io.codemodder.codemods;
 
+import com.contrastsecurity.sarif.PhysicalLocation;
 import com.contrastsecurity.sarif.Result;
 import io.codemodder.*;
 import io.codemodder.providers.sarif.codeql.CodeQLScan;
+import io.github.pixee.security.BoundedLineReader;
+import io.github.pixee.security.XMLInputFactorySecurity;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
+import javax.xml.stream.XMLEventFactory;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.events.XMLEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Fixes issues reported under the id "java/maven-non-https-url". */
 @Codemod(
@@ -18,9 +38,129 @@ public class MavenSecureURLCodemod extends SarifPluginRawFileChanger {
     super(sarif);
   }
 
+  private Set<PhysicalLocation> getLocationsWithinFile(
+      final File target, final List<Result> results, final String repositoryRootPath) {
+    return results.stream()
+        .map(result -> result.getLocations().get(0).getPhysicalLocation())
+        .filter(
+            pl ->
+                Path.of(repositoryRootPath, pl.getArtifactLocation().getUri())
+                    .equals(target.toPath()))
+        .collect(Collectors.toUnmodifiableSet());
+  }
+
+  private static boolean isWithinPhysicalLocation(
+      final Set<PhysicalLocation> locations, final XMLEvent xmlEvent) {
+    final var xmlLocation = xmlEvent.getLocation();
+    return locations.stream()
+        .anyMatch(
+            pl ->
+                xmlLocation.getLineNumber() >= pl.getRegion().getStartLine()
+                    && xmlLocation.getLineNumber() <= pl.getRegion().getEndLine()
+                    && xmlLocation.getColumnNumber() >= pl.getRegion().getStartColumn()
+                    && xmlLocation.getColumnNumber() <= pl.getRegion().getEndColumn());
+  }
+
+  private int secondEventOffset(File file, XMLInputFactory xmlInputFactory) throws Exception {
+    try (FileInputStream fileIS = new FileInputStream(file)) {
+      final var xmlReader = xmlInputFactory.createXMLEventReader(fileIS);
+      xmlReader.nextEvent();
+      var second = xmlReader.nextEvent();
+      return second.getLocation().getCharacterOffset();
+    }
+  }
+
   @Override
   public List<Weave> onFileFound(
       final CodemodInvocationContext context, final List<Result> results) {
-    return List.of();
+    // Not even an xml
+    File file = context.path().toFile();
+    // No suggested changes within file
+    final var locations = getLocationsWithinFile(file, results, context.codeDirectory().asPath().toString());
+    if (locations.isEmpty()) {
+      return List.of();
+    }
+
+    List<Weave> allWeaves = new ArrayList<>();
+
+    final var xmlInputFactory =
+        XMLInputFactorySecurity.hardenFactory(XMLInputFactory.newInstance());
+    final var xmlOutputFactory = XMLOutputFactory.newInstance();
+    final var xmlEventFactory = XMLEventFactory.newInstance();
+    try {
+      final File tempFile =
+          File.createTempFile(file.getName(), getExtension(file.getName()).orElse(".tmp"));
+      try (FileInputStream fileIS = new FileInputStream(file);
+          FileWriter fileWriter = new FileWriter(tempFile)) {
+        final var xmlReader = xmlInputFactory.createXMLEventReader(fileIS);
+        final var xmlWriter = xmlOutputFactory.createXMLEventWriter(fileWriter);
+        while (xmlReader.hasNext()) {
+          final var nextEvent = xmlReader.nextEvent();
+          xmlWriter.add(nextEvent);
+          // We search for any url tag and check if it's within any physical location
+          if (nextEvent.isStartElement()
+              && nextEvent.asStartElement().getName().getLocalPart().equals("url")
+              && isWithinPhysicalLocation(locations, nextEvent)) {
+            final var startElement = nextEvent.asStartElement();
+            final var url = xmlReader.nextEvent().asCharacters().getData();
+            final var line = startElement.getLocation().getLineNumber();
+            if (url.startsWith("http:")) {
+              final var fixed = "https:" + url.substring(5);
+              xmlWriter.add(xmlEventFactory.createCharacters(fixed));
+              final var weave = Weave.from(line, context.codemodId());
+              allWeaves.add(weave);
+            } else if (url.startsWith("ftp:")) {
+              final var fixed = "ftps:" + url.substring(4);
+              xmlWriter.add(xmlEventFactory.createCharacters(fixed));
+              final var weave = Weave.from(line, context.codemodId());
+              allWeaves.add(weave);
+            } else {
+              xmlWriter.add(startElement);
+            }
+          }
+        }
+      }
+      if (allWeaves.isEmpty()) {
+        return List.of();
+      }
+      // Workaround for a bug (?) where the whitespaces between prolog and the first tag are ignored
+      // Get all the characters until the first 2 events
+      // The first one will be the prolog, if it exists
+      var originalOffset = secondEventOffset(file, xmlInputFactory);
+      var tempOffset = secondEventOffset(tempFile, xmlInputFactory);
+
+      // We want to glue the "head" of the original file with the tail of the tempFile
+      var originalHead = "";
+      try (FileInputStream fileIS = new FileInputStream(file)) {
+        originalHead = new String(fileIS.readNBytes(originalOffset));
+      }
+
+      final File modifiedFile =
+          File.createTempFile(file.getName(), getExtension(file.getName()).orElse(".tmp"));
+      try (BufferedReader reader = new BufferedReader(new FileReader(tempFile));
+          BufferedWriter writer = new BufferedWriter(new FileWriter(modifiedFile))) {
+        reader.skip(tempOffset);
+        writer.append(originalHead);
+        while (reader.ready()) {
+          writer.write(BoundedLineReader.readLine(reader, 1000000));
+        }
+      }
+      tempFile.deleteOnExit();
+
+      return allWeaves;
+    } catch (final Exception e) {
+      LOG.error("Problem handling file: {}", file.getPath(), e);
+      return List.of();
+    }
   }
+
+  private static Optional<String> getExtension(final String name) {
+    final int dotIndex = name.indexOf('.');
+    if (dotIndex != name.length() - 1) {
+      return Optional.of(name.substring(dotIndex));
+    }
+    return Optional.of(name);
+  }
+
+  private static final Logger LOG = LoggerFactory.getLogger(MavenSecureURLCodemod.class);
 }
--- codemodder-community-codemods/src/test/java/io/codemodder/codemods/MavenSecureURLCodemodTest.java
+++ codemodder-community-codemods/src/test/java/io/codemodder/codemods/MavenSecureURLCodemodTest.java
@@ -3,7 +3,11 @@ package io.codemodder.codemods;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
+import com.github.difflib.DiffUtils;
+import com.github.difflib.patch.AbstractDelta;
+import com.github.difflib.patch.Patch;
 import io.codemodder.*;
 import java.io.File;
 import java.io.IOException;
@@ -18,30 +22,70 @@ import org.junit.jupiter.api.io.TempDir;
 
 public class MavenSecureURLCodemodTest {
 
-  @Test
-  void it_changes_http_to_https(final @TempDir Path tmpDir) throws IOException {
+  private Optional<ChangedFile> invokeAndGetChanges(final String pomFilename, final Path tmpDir) throws IOException{
     String dir = "src/test/resources/maven-non-https-url/";
     copyDir(Path.of(dir), tmpDir);
 
     List<File> allSarifs = new ArrayList<>();
     Files.newDirectoryStream(tmpDir, "*.sarif")
-        .iterator()
-        .forEachRemaining(p -> allSarifs.add(p.toFile()));
+            .iterator()
+            .forEachRemaining(p -> allSarifs.add(p.toFile()));
     Map<String, List<RuleSarif>> map = new SarifParser.Default().parseIntoMap(allSarifs, tmpDir);
 
     CodemodInvoker codemodInvoker =
-        new CodemodInvoker(List.of(MavenSecureURLCodemod.class), tmpDir, map);
-    Path pom = tmpDir.resolve("pom_insecure_url_1.xml");
+            new CodemodInvoker(List.of(MavenSecureURLCodemod.class), tmpDir, map);
+    Path pom = tmpDir.resolve(pomFilename);
     FileWeavingContext context =
-        FileWeavingContext.createDefault(pom.toFile(), IncludesExcludes.any());
-    Optional<ChangedFile> changedFileOptional = codemodInvoker.executeFile(pom, context);
+            FileWeavingContext.createDefault(pom.toFile(), IncludesExcludes.any());
+    return codemodInvoker.executeFile(pom, context);
+  }
 
+  @Test
+  void it_changes_http_to_https(final @TempDir Path tmpDir) throws IOException {
+    String pomFilename = "pom_insecure_url_1.xml";
+    Optional<ChangedFile> changedFileOptional = invokeAndGetChanges(pomFilename,tmpDir);
     assertThat(
         changedFileOptional.stream()
             .flatMap(cf -> cf.weaves().stream())
             .map(Weave::lineNumber)
             .collect(Collectors.toList()),
         contains(26));
+  }
+
+  @Test
+  void it_changes_ftp_to_sftp(final @TempDir Path tmpDir) throws IOException {
+    String pomFilename = "pom_insecure_url_2.xml";
+    Optional<ChangedFile> changedFileOptional = invokeAndGetChanges(pomFilename,tmpDir);
+    assertThat(
+            changedFileOptional.stream()
+                    .flatMap(cf -> cf.weaves().stream())
+                    .map(Weave::lineNumber)
+                    .collect(Collectors.toList()),
+            contains(26));
+  }
+
+  @Test
+  void it_preserves_whitespaces(final @TempDir Path tmpDir) throws IOException {
+    String pomFilename = "pom_insecure_url_wonky.xml";
+    Optional<ChangedFile> changedFileOptional = invokeAndGetChanges(pomFilename,tmpDir);
+
+    var originalLines = Files.readAllLines(tmpDir.resolve(Path.of(pomFilename)));
+    var changedLines = Files.readAllLines(new File(changedFileOptional.get().modifiedFile()).toPath());
+    var deltas = getDeltas(originalLines, changedLines);
+
+    assertThat(deltas.size(), lessThanOrEqualTo(1));
+    assertThat(
+            changedFileOptional.stream()
+                    .flatMap(cf -> cf.weaves().stream())
+                    .map(Weave::lineNumber)
+                    .collect(Collectors.toList()),
+            contains(34));
+  }
+
+  private static List<AbstractDelta<String>> getDeltas(
+          final List<String> originalLines, final List<String> comparedLines) {
+    final Patch<String> patch = DiffUtils.diff(originalLines, comparedLines);
+    return patch.getDeltas();
   }
 
   private static void copyDir(Path src, Path dest) throws IOException {
