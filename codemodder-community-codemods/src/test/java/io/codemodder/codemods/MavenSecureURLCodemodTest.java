c37d6055f213a9d319bb64eeacfc1b656ce8eba4
andrecs <12188364+andrecsilva@users.noreply.github.com>
2023-04-21 09:50:16 -0300

    Spotless

--- codemodder-community-codemods/src/test/java/io/codemodder/codemods/MavenSecureURLCodemodTest.java
+++ codemodder-community-codemods/src/test/java/io/codemodder/codemods/MavenSecureURLCodemodTest.java
@@ -1,11 +1,10 @@
 package io.codemodder.codemods;
 
-import io.codemodder.*;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 
+import io.codemodder.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,54 +13,51 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.*;
 import java.util.stream.Collectors;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class MavenSecureURLCodemodTest {
 
-    @Test
-    void it_changes_http_to_https(
-            final @TempDir Path tmpDir)
-            throws IOException {
-        String dir = "src/test/resources/maven-non-https-url/";
-        copyDir(Path.of(dir), tmpDir);
+  @Test
+  void it_changes_http_to_https(final @TempDir Path tmpDir) throws IOException {
+    String dir = "src/test/resources/maven-non-https-url/";
+    copyDir(Path.of(dir), tmpDir);
 
-        List<File> allSarifs = new ArrayList<>();
-        Files.newDirectoryStream(tmpDir, "*.sarif")
-                .iterator()
-                .forEachRemaining(p -> allSarifs.add(p.toFile()));
-        Map<String, List<RuleSarif>> map = new SarifParser.Default().parseIntoMap(allSarifs, tmpDir);
+    List<File> allSarifs = new ArrayList<>();
+    Files.newDirectoryStream(tmpDir, "*.sarif")
+        .iterator()
+        .forEachRemaining(p -> allSarifs.add(p.toFile()));
+    Map<String, List<RuleSarif>> map = new SarifParser.Default().parseIntoMap(allSarifs, tmpDir);
 
-        CodemodInvoker codemodInvoker = new CodemodInvoker(List.of(MavenSecureURLCodemod.class), tmpDir,map);
-        Path pom = tmpDir.resolve("pom_insecure_url_1.xml");
-        FileWeavingContext context =
-                FileWeavingContext.createDefault(pom.toFile(), IncludesExcludes.any());
-        Optional<ChangedFile> changedFileOptional = codemodInvoker.executeFile(pom, context);
+    CodemodInvoker codemodInvoker =
+        new CodemodInvoker(List.of(MavenSecureURLCodemod.class), tmpDir, map);
+    Path pom = tmpDir.resolve("pom_insecure_url_1.xml");
+    FileWeavingContext context =
+        FileWeavingContext.createDefault(pom.toFile(), IncludesExcludes.any());
+    Optional<ChangedFile> changedFileOptional = codemodInvoker.executeFile(pom, context);
 
-        assertThat(
-                changedFileOptional.stream()
-                        .flatMap(cf -> cf.weaves().stream())
-                        .map(Weave::lineNumber)
-                        .collect(Collectors.toList()),
-                contains(26));
-    }
+    assertThat(
+        changedFileOptional.stream()
+            .flatMap(cf -> cf.weaves().stream())
+            .map(Weave::lineNumber)
+            .collect(Collectors.toList()),
+        contains(26));
+  }
 
-    private static void copyDir(Path src, Path dest) throws IOException {
-        String srcPath = src.toString();
-        String destPath = dest.toString();
-        Files.walk(Paths.get(srcPath))
-                .forEach(
-                        a -> {
-                            Path b = Paths.get(destPath, a.toString().substring(srcPath.length()));
-                            if (!a.toString().equals(srcPath)) {
-                                try {
-                                    Files.copy(a, b, StandardCopyOption.REPLACE_EXISTING);
-                                } catch (IOException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            }
-                        });
-    }
+  private static void copyDir(Path src, Path dest) throws IOException {
+    String srcPath = src.toString();
+    String destPath = dest.toString();
+    Files.walk(Paths.get(srcPath))
+        .forEach(
+            a -> {
+              Path b = Paths.get(destPath, a.toString().substring(srcPath.length()));
+              if (!a.toString().equals(srcPath)) {
+                try {
+                  Files.copy(a, b, StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                  throw new RuntimeException(e);
+                }
+              }
+            });
+  }
 }
--- framework/codemodder-core/src/main/java/io/codemodder/SarifPluginRawFileChanger.java
+++ framework/codemodder-core/src/main/java/io/codemodder/SarifPluginRawFileChanger.java
@@ -1,27 +1,26 @@
 package io.codemodder;
 
 import com.contrastsecurity.sarif.Result;
-
 import java.io.IOException;
 import java.util.List;
 
-/** A {@link RawFileChanger} bundled with a {@link RuleSarif}.*/
-public abstract class SarifPluginRawFileChanger implements RawFileChanger{
+/** A {@link RawFileChanger} bundled with a {@link RuleSarif}. */
+public abstract class SarifPluginRawFileChanger implements RawFileChanger {
 
-    private final RuleSarif sarif;
+  private final RuleSarif sarif;
 
-    protected SarifPluginRawFileChanger(final RuleSarif sarif){
-        this.sarif = sarif;
+  protected SarifPluginRawFileChanger(final RuleSarif sarif) {
+    this.sarif = sarif;
+  }
+
+  @Override
+  public void visitFile(final CodemodInvocationContext context) throws IOException {
+    List<Result> results = sarif.getResultsByPath(context.path());
+    if (!results.isEmpty()) {
+      List<Weave> allChanges = onFileFound(context, results);
+      allChanges.stream().forEach(weave -> context.changeRecorder().addWeave(weave));
     }
+  }
 
-    @Override
-    public void visitFile(final CodemodInvocationContext context) throws IOException {
-        List<Result> results = sarif.getResultsByPath(context.path());
-        if(!results.isEmpty()) {
-            List<Weave> allChanges = onFileFound(context, results);
-            allChanges.stream().forEach( weave -> context.changeRecorder().addWeave(weave));
-        }
-    }
-
-    public abstract List<Weave> onFileFound(CodemodInvocationContext context, List<Result> results);
+  public abstract List<Weave> onFileFound(CodemodInvocationContext context, List<Result> results);
 }
