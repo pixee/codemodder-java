127d6dd6c69b122a3465b619895bf80b4af1614f
Aldrin Leal <aldrin@leal.eng.br>
2023-05-05 19:13:14 -0500

    Cleans up dependencies

--- plugins/codemodder-plugin-maven/build.gradle.kts
+++ plugins/codemodder-plugin-maven/build.gradle.kts
@@ -13,20 +13,7 @@ java {
 dependencies {
     compileOnly(libs.jetbrains.annotations)
     implementation("io.codemodder:codemodder-core")
-    implementation("io.openpixee.maven:pom-operator:0.0.6") // TODO bring into monorepo
-    {
-        exclude(group = "com.google.inject", module = "guice")
-    }
-
-    implementation("org.apache.maven.resolver:maven-resolver-api:1.9.2")
-    implementation("org.apache.maven.resolver:maven-resolver-spi:1.9.2")
-    implementation("org.apache.maven.resolver:maven-resolver-util:1.9.2")
-    implementation("org.apache.maven.resolver:maven-resolver-impl:1.9.2")
-    implementation("org.apache.maven.resolver:maven-resolver-transport-file:1.9.2")
-    implementation("org.apache.maven.resolver:maven-resolver-transport-http:1.9.2")
-    implementation("org.apache.maven.resolver:maven-resolver-connector-basic:1.9.2")
-    implementation("org.apache.maven:maven-resolver-provider:3.8.6")
-    implementation("org.apache.maven:maven-model-builder:3.8.6")
+    implementation("io.openpixee.maven:pom-operator:0.0.7") // TODO bring into monorepo
 
     testImplementation(testlibs.bundles.junit.jupiter)
     testImplementation(testlibs.bundles.hamcrest)
