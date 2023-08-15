c3e3450300d738c2c244f67fa2d2085a7251fd0a
Ryan Dens <ryan.dens@pixee.ai>
2023-08-15 12:41:29 -0700

    :construction: enable non-pixee employees to build this repository

--- gradle/build-settings/src/main/kotlin/io.codemodder.repositories.settings.gradle.kts
+++ gradle/build-settings/src/main/kotlin/io.codemodder.repositories.settings.gradle.kts
@@ -1,9 +1,14 @@
 dependencyResolutionManagement {
     repositories {
-        maven {
-            name = "pixeeArtifactory"
-            url = uri("https://pixee.jfrog.io/artifactory/default-maven-virtual")
-            credentials(PasswordCredentials::class)
+        if (providers.gradleProperty("pixeeBuild").isPresent) {
+            maven {
+                name = "pixeeArtifactory"
+                url = uri("https://pixee.jfrog.io/artifactory/default-maven-virtual")
+                credentials(PasswordCredentials::class)
+            }
+        } else {
+            mavenCentral()
+            gradlePluginPortal()
         }
     }
 }
