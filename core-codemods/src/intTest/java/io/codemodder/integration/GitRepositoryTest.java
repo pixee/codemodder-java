f814107d466707e703fe2dc76eb7ca2ab55a5b43
Aldrin Leal <aldrin@leal.eng.br>
2023-08-16 23:39:39 -0500

    Fixing formatting

--- core-codemods/src/intTest/java/io/codemodder/integration/GitRepositoryTest.java
+++ core-codemods/src/intTest/java/io/codemodder/integration/GitRepositoryTest.java
@@ -7,7 +7,6 @@ import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.Objects;
 import java.util.stream.Stream;
-
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.ResetCommand;
