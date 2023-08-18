b5b190d3e7bd6dc9580982f4b47e1cb3b5adb38f
Aldrin Leal <aldrin@leal.eng.br>
2023-08-18 10:33:20 -0500

    Making it more random

--- plugins/codemodder-plugin-maven/src/main/java/io/codemodder/plugins/maven/MavenProvider.java
+++ plugins/codemodder-plugin-maven/src/main/java/io/codemodder/plugins/maven/MavenProvider.java
@@ -4,6 +4,7 @@ import com.github.difflib.DiffUtils;
 import com.github.difflib.UnifiedDiffUtils;
 import com.github.difflib.patch.AbstractDelta;
 import com.github.difflib.patch.Patch;
+
 import io.codemodder.DependencyGAV;
 import io.codemodder.DependencyUpdateResult;
 import io.codemodder.ProjectProvider;
@@ -14,7 +15,9 @@ import io.github.pixee.maven.operator.POMDocument;
 import io.github.pixee.maven.operator.POMOperator;
 import io.github.pixee.maven.operator.POMScanner;
 import io.github.pixee.maven.operator.ProjectModel;
+import io.github.pixee.maven.operator.ProjectModelFactory;
 import io.github.pixee.maven.operator.QueryType;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -34,6 +37,7 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.VisibleForTesting;
 
@@ -55,26 +59,32 @@ import org.jetbrains.annotations.VisibleForTesting;
  */
 public final class MavenProvider implements ProjectProvider {
 
-  /** Represents a failure when doing a dependency update. */
+  /**
+   * Represents a failure when doing a dependency update.
+   */
   static class DependencyUpdateException extends RuntimeException {
     private DependencyUpdateException(String message, Throwable cause) {
       super(message, cause);
     }
   }
 
-  /** A seam for handling writing poms to disk. */
+  /**
+   * A seam for handling writing poms to disk.
+   */
   interface PomModifier {
     /**
      * Modifies a POM writing back its contents
      *
-     * @param path where to write
+     * @param path     where to write
      * @param contents contents to write
      * @throws IOException failure when writing
      */
     void modify(final Path path, final byte[] contents) throws IOException;
   }
 
-  /** Default Implementation of Pom Modifier Interface */
+  /**
+   * Default Implementation of Pom Modifier Interface
+   */
   static class DefaultPomModifier implements PomModifier {
     @Override
     public void modify(final Path path, final byte[] contents) throws IOException {
@@ -192,12 +202,24 @@ public final class MavenProvider implements ProjectProvider {
 
           System.out.println("Need to inject it...");
 
+          ProjectModelFactory projectModelFactory = POMScanner.legacyScanFrom(pomFile.toFile(), projectDir.toFile())
+              .withDependency(newDependency)
+              .withSkipIfNewer(true)
+              .withUseProperties(true)
+              .withOffline(this.offline);
+
+          if (this.offline) {
+            try {
+              projectModelFactory =
+                  projectModelFactory
+                      .withRepositoryPath(Files.createTempDirectory(null).toFile());
+            } catch (IOException e) {
+              throw new RuntimeException(e);
+            }
+          }
+
           ProjectModel projectModel =
-              POMScanner.legacyScanFrom(pomFile.toFile(), projectDir.toFile())
-                  .withDependency(newDependency)
-                  .withSkipIfNewer(true)
-                  .withUseProperties(true)
-                  .withOffline(this.offline)
+              projectModelFactory
                   .build();
 
           boolean result = POMOperator.modify(projectModel);
@@ -285,10 +307,22 @@ public final class MavenProvider implements ProjectProvider {
 
   @NotNull
   private Collection<DependencyGAV> getDependenciesFrom(Path pomFile, Path projectDir) {
+    ProjectModelFactory projectModelFactory = POMScanner.legacyScanFrom(pomFile.toFile(), projectDir.toFile())
+        .withQueryType(QueryType.SAFE)
+        .withOffline(true);
+
+    if (this.offline) {
+      try {
+        projectModelFactory =
+            projectModelFactory
+                .withRepositoryPath(Files.createTempDirectory(null).toFile());
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
     ProjectModel originalProjectModel =
-        POMScanner.legacyScanFrom(pomFile.toFile(), projectDir.toFile())
-            .withQueryType(QueryType.SAFE)
-            .withOffline(true)
+        projectModelFactory
             .build();
 
     Collection<Dependency> foundDependencies = POMOperator.queryDependency(originalProjectModel);
