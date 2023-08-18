e3686194204d87bb002ec058f9af8745b83e8970
Ryan Dens <ryan.dens@pixee.ai>
2023-08-18 10:06:14 -0700

    :chart_with_upwards_trend: publish build scans on CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,25 @@ dependencyResolutionManagement {
 
 plugins {
     id("io.codemodder.repositories")
+    id("com.gradle.enterprise") version "3.14.1"
+}
+
+val isCI = providers.environmentVariable("CI").isPresent
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        isUploadInBackground = !isCI
+
+        if (isCI) {
+            publishAlways()
+        }
+
+        capture {
+            isTaskInputFiles = true
+        }
+    }
 }
 
 includeBuild("core-codemods")
