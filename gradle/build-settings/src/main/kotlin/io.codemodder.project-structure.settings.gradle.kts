955493e2befda5c67484a50f23afc3fdfe2696d7
Ryan Dens <ryan.dens@pixee.ai>
2023-08-18 13:27:32 -0700

    :fire: remove unused plugins

--- gradle/build-settings/src/main/kotlin/io.codemodder.project-structure.settings.gradle.kts
+++
@@ -1,6 +0,0 @@
-// this plugin gives us one place to modify the strategy we use to structure subprojects
-
-// this strategy automatically includes all projects that are direct children of the root directory
-rootDir.listFiles()?.filter { File(it, "build.gradle.kts").exists() }?.forEach { subproject ->
-    include(subproject.name)
-}
