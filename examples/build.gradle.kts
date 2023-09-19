6c8c578ae45daac5942b32430ebfdedcbb7ed338
Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>
2023-09-18 21:57:13 -0400

    removed unneeded deps

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -9,16 +9,10 @@ description = "Example codemods"
 dependencies {
     implementation(project(":framework:codemodder-base"))
     implementation(project(":plugins:codemodder-plugin-semgrep"))
-    implementation(project(":plugins:codemodder-plugin-codeql"))
-    implementation(project(":plugins:codemodder-plugin-maven"))
-    implementation(project(":plugins:codemodder-plugin-llm"))
-    implementation(project(":plugins:codemodder-plugin-aws"))
-    implementation(project(":plugins:codemodder-plugin-pmd"))
     testImplementation(testlibs.bundles.junit.jupiter)
     testImplementation(testlibs.bundles.hamcrest)
     testImplementation(testlibs.assertj)
     testImplementation(testlibs.mockito)
     testImplementation(project(":framework:codemodder-testutils"))
-    testImplementation(project(":framework:codemodder-testutils-llm"))
     testRuntimeOnly(testlibs.junit.jupiter.engine)
 }
