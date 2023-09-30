d81543522f260d6e4a1bab0c60118f7aad7dc025
Arturo <sergioarturoespinoza@gmail.com>
2023-09-30 15:02:25 -0600

    Testing the app container creation from dockerfile

---
+++ core-codemods/src/test/resources/test-applications/demo/src/main/java/com/example/demo/api/TestController.java
@@ -0,0 +1,44 @@
+package com.example.demo.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String switchTest(@RequestParam final int day) {
+
+        final String dayString = switch (day) {
+
+            // Case
+            case 1 -> "Monday";
+
+            // Case
+            case 2 -> "Tuesday";
+
+            // Case
+            case 3 -> "Wednesday";
+
+            // Case
+            case 4 -> "Thursday";
+
+            // Case
+            case 5 -> "Friday";
+
+            // Case
+            case 6 -> "Saturday";
+
+            // Case
+            case 7 -> "Sunday";
+
+            // Default case
+            default -> "Invalid day";
+        };
+
+      return dayString;
+    }
+
+}
