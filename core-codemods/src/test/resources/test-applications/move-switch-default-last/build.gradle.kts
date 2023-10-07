fac345e4ad4ba4e6a6db6652d2874ac0ae8376ad
Arturo <sergioarturoespinoza@gmail.com>
2023-10-07 16:02:31 -0600

    adding project image to speed uo the jar building in the main image

--- core-codemods/src/test/resources/test-applications/move-switch-default-last/build.gradle.kts
+++ core-codemods/src/test/resources/test-applications/move-switch-default-last/build.gradle.kts
@@ -17,10 +17,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework:spring-web:6.0.12")
-    implementation("org.springframework.boot:spring-boot-autoconfigure:3.1.4")
-    implementation("org.springframework.boot:spring-boot-starter-tomcat:3.1.4")
-    implementation("org.springframework:spring-webmvc:6.0.12")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.1.4")
 }
 
 tasks.named<BootJar>("bootJar") {
