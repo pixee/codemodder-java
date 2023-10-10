54495d69c758cfc87e96adc6e2c33215805c4e45
Carlos Uscanga <carlos.07um@gmail.com>
2023-10-09 18:28:38 -0600

    versions

--- plugins/codemodder-plugin-maven/build.gradle.kts
+++ plugins/codemodder-plugin-maven/build.gradle.kts
@@ -11,6 +11,32 @@ repositories {
 description = "Plugin for providing Maven dependency management functions to codemods."
 
 dependencies {
+    val docoptVersion = "0.6.0.20150202"
+    val commonsLangVersion = "3.12.0"
+    val dom4jVersion = "2.1.3"
+    val jaxenVersion = "1.2.0"
+    val xercesImplVersion = "2.12.2"
+    val xmlUnitVersion = "2.9.0"
+    val kotlinVersion = "1.7.10"
+    val javaSemverVersion = "0.9.0"
+    val commonsIoVersion = "2.11.0"
+    val mavenInvokerVersion = "3.2.0"
+    val mavenEmbedderVersion = "3.8.6"
+    val guiceVersion = "5.1.0"
+    val mavenResolverVersion = "1.9.2"
+    val mavenProviderVersion = "3.8.6"
+    val modelBuilderVersion = "3.8.6"
+    val juniversalchardetVersion = "2.4.0"
+    val commonsCollections4Version = "4.1"
+    val slf4jSimpleVersion = "2.0.0"
+    val diffMatchPatchVersion = "0.0.2"
+    val javaDiffUtilsVersion = "4.12"
+    val hamcrestVersion = "1.3"
+    val junitVersion = "4.13.2"
+    val kotlinTestVersion = "1.7.10"
+    val slf4jApiVersion = "2.0.0"
+    val lombokVersion = "1.18.30"
+
     compileOnly(libs.jetbrains.annotations)
     implementation(project(":framework:codemodder-base"))
 
@@ -21,44 +47,44 @@ dependencies {
     testImplementation(testlibs.mockito)
     testRuntimeOnly(testlibs.junit.jupiter.engine)
 
-    implementation("com.offbytwo:docopt:0.6.0.20150202")
-    implementation("org.apache.commons:commons-lang3:3.12.0")
-    implementation("org.dom4j:dom4j:2.1.3")
-    implementation("jaxen:jaxen:1.2.0")
-    implementation("xerces:xercesImpl:2.12.2")
-    implementation("org.xmlunit:xmlunit-core:2.9.0")
-    implementation("org.xmlunit:xmlunit-assertj3:2.9.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
-    implementation("com.github.zafarkhaja:java-semver:0.9.0")
-    implementation("commons-io:commons-io:2.11.0")
-    implementation("org.apache.maven.shared:maven-invoker:3.2.0")
-    implementation("org.apache.maven:maven-embedder:3.8.6") {
+    implementation("com.offbytwo:docopt:$docoptVersion")
+    implementation("org.apache.commons:commons-lang3:$commonsLangVersion")
+    implementation("org.dom4j:dom4j:$dom4jVersion")
+    implementation("jaxen:jaxen:$jaxenVersion")
+    implementation("xerces:xercesImpl:$xercesImplVersion")
+    implementation("org.xmlunit:xmlunit-core:$xmlUnitVersion")
+    implementation("org.xmlunit:xmlunit-assertj3:$xmlUnitVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("com.github.zafarkhaja:java-semver:$javaSemverVersion")
+    implementation("commons-io:commons-io:$commonsIoVersion")
+    implementation("org.apache.maven.shared:maven-invoker:$mavenInvokerVersion")
+    implementation("org.apache.maven:maven-embedder:$mavenEmbedderVersion") {
         exclude(group = "com.google.inject", module = "guice")
     }
-    implementation("org.apache.maven:maven-compat:3.8.6") {
+    implementation("org.apache.maven:maven-compat:$mavenEmbedderVersion") {
         exclude(group = "com.google.inject", module = "guice")
     }
-    implementation("com.google.inject:guice:5.1.0")
-    implementation("org.apache.maven.resolver:maven-resolver-api:1.9.2")
-    implementation("org.apache.maven.resolver:maven-resolver-spi:1.9.2")
-    implementation("org.apache.maven.resolver:maven-resolver-util:1.9.2")
-    implementation("org.apache.maven.resolver:maven-resolver-impl:1.9.2")
-    implementation("org.apache.maven.resolver:maven-resolver-transport-file:1.9.2")
-    implementation("org.apache.maven.resolver:maven-resolver-transport-http:1.9.2")
-    implementation("org.apache.maven.resolver:maven-resolver-connector-basic:1.9.2")
-    implementation("org.apache.maven:maven-resolver-provider:3.8.6")
-    implementation("org.apache.maven:maven-model-builder:3.8.6")
-    implementation("com.github.albfernandez:juniversalchardet:2.4.0")
-    implementation("org.apache.commons:commons-collections4:4.1")
-    testImplementation("org.slf4j:slf4j-simple:2.0.0")
-    testImplementation("fun.mike:diff-match-patch:0.0.2")
-    testImplementation("io.github.java-diff-utils:java-diff-utils:4.12")
-    testImplementation("org.hamcrest:hamcrest-all:1.3")
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.7.10")
-    compileOnly("org.slf4j:slf4j-api:2.0.0")
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
-    testCompileOnly("org.projectlombok:lombok:1.18.30")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
+    implementation("com.google.inject:guice:$guiceVersion")
+    implementation("org.apache.maven.resolver:maven-resolver-api:$mavenResolverVersion")
+    implementation("org.apache.maven.resolver:maven-resolver-spi:$mavenResolverVersion")
+    implementation("org.apache.maven.resolver:maven-resolver-util:$mavenResolverVersion")
+    implementation("org.apache.maven.resolver:maven-resolver-impl:$mavenResolverVersion")
+    implementation("org.apache.maven.resolver:maven-resolver-transport-file:$mavenResolverVersion")
+    implementation("org.apache.maven.resolver:maven-resolver-transport-http:$mavenResolverVersion")
+    implementation("org.apache.maven.resolver:maven-resolver-connector-basic:$mavenResolverVersion")
+    implementation("org.apache.maven:maven-resolver-provider:$mavenProviderVersion")
+    implementation("org.apache.maven:maven-model-builder:$modelBuilderVersion")
+    implementation("com.github.albfernandez:juniversalchardet:$juniversalchardetVersion")
+    implementation("org.apache.commons:commons-collections4:$commonsCollections4Version")
+    testImplementation("org.slf4j:slf4j-simple:$slf4jSimpleVersion")
+    testImplementation("fun.mike:diff-match-patch:$diffMatchPatchVersion")
+    testImplementation("io.github.java-diff-utils:java-diff-utils:$javaDiffUtilsVersion")
+    testImplementation("org.hamcrest:hamcrest-all:$hamcrestVersion")
+    testImplementation("junit:junit:$junitVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinTestVersion")
+    compileOnly("org.slf4j:slf4j-api:$slf4jApiVersion")
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+    testCompileOnly("org.projectlombok:lombok:$lombokVersion")
+    testAnnotationProcessor("org.projectlombok:lombok:$lombokVersion")
 }
