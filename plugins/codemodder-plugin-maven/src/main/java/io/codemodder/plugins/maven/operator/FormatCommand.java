ce1ef65055f7bbc3e0fe4a74fdce61ef4a5f17ee
Carlos Uscanga <carlos.07um@gmail.com>
2023-10-11 12:30:08 -0600

    sync to pom-operator last changes

--- plugins/codemodder-plugin-maven/src/main/java/io/codemodder/plugins/maven/operator/FormatCommand.java
+++ plugins/codemodder-plugin-maven/src/main/java/io/codemodder/plugins/maven/operator/FormatCommand.java
@@ -477,13 +477,8 @@ public class FormatCommand extends AbstractCommand {
       if (null == representationMatch) {
         LOGGER.warn("Failure on quoting: {}", match);
       } else {
-        int start = representationMatch.getRange().getStart();
-        int end = representationMatch.getRange().getLast();
-
         xmlRepresentation =
-            xmlRepresentation.substring(0, start)
-                + match.getContent()
-                + xmlRepresentation.substring(end);
+            replaceRange(xmlRepresentation, representationMatch.getRange(), match.getContent());
         lastIndex = representationMatch.getRange().getFirst() + match.getContent().length();
       }
     }
