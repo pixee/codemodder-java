9362a773b4d58de9bdff7d43865ef4f0e346f256
Carlos Uscanga <carlos.07um@gmail.com>
2023-10-12 13:29:31 -0600

    WrongDependencyTypeException javadoc

--- plugins/codemodder-plugin-maven/src/main/java/io/codemodder/plugins/maven/operator/WrongDependencyTypeException.java
+++ plugins/codemodder-plugin-maven/src/main/java/io/codemodder/plugins/maven/operator/WrongDependencyTypeException.java
@@ -1,6 +1,13 @@
 package io.codemodder.plugins.maven.operator;
 
+/** An exception that indicates a wrong dependency type was encountered. */
 class WrongDependencyTypeException extends RuntimeException {
+
+  /**
+   * Constructs a new WrongDependencyTypeException with the specified error message.
+   *
+   * @param message The error message describing the nature of the exception.
+   */
   public WrongDependencyTypeException(String message) {
     super(message);
   }
