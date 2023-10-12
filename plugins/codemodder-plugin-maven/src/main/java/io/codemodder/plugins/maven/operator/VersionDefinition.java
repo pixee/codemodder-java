f1e0094c5f2337e5166fa85ca90188c2686e5fad
Carlos Uscanga <carlos.07um@gmail.com>
2023-10-12 13:28:18 -0600

    VersionDefinition javadoc

--- plugins/codemodder-plugin-maven/src/main/java/io/codemodder/plugins/maven/operator/VersionDefinition.java
+++ plugins/codemodder-plugin-maven/src/main/java/io/codemodder/plugins/maven/operator/VersionDefinition.java
@@ -1,14 +1,22 @@
 package io.codemodder.plugins.maven.operator;
 
 /**
- * Represents a tuple (kind / version string) applicable from a pom.xml file
+ * Represents a tuple containing version information (kind / version string) applicable to a pom.xml
+ * file.
  *
- * <p>For Internal Consumption (thus Internal)
+ * <p>This class is intended for internal consumption and should not be used directly by external
+ * code.
  */
 class VersionDefinition {
   private Kind kind;
   private String value;
 
+  /**
+   * Constructs a new VersionDefinition with the specified kind and version string.
+   *
+   * @param kind The kind of version information.
+   * @param value The version string.
+   */
   public VersionDefinition(Kind kind, String value) {
     this.kind = kind;
     this.value = value;
