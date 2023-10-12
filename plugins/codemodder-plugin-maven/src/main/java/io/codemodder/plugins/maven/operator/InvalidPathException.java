3d3a801ce1ff043183b41faa2e9e4e79d522e233
Carlos Uscanga <carlos.07um@gmail.com>
2023-10-12 12:48:39 -0600

    InvalidPathException javadoc

--- plugins/codemodder-plugin-maven/src/main/java/io/codemodder/plugins/maven/operator/InvalidPathException.java
+++ plugins/codemodder-plugin-maven/src/main/java/io/codemodder/plugins/maven/operator/InvalidPathException.java
@@ -3,11 +3,19 @@ package io.codemodder.plugins.maven.operator;
 import java.io.File;
 import java.io.IOException;
 
+/** Exception class for handling invalid file paths. */
 class InvalidPathException extends IOException {
   private final File parentPath;
   private final String relativePath;
   private final boolean loop;
 
+  /**
+   * Constructs an InvalidPathException with the specified details.
+   *
+   * @param parentPath The parent directory path.
+   * @param relativePath The relative path that is considered invalid.
+   * @param loop Indicates whether the path forms a loop.
+   */
   public InvalidPathException(File parentPath, String relativePath, boolean loop) {
     super(
         "Invalid Relative Path "
