f84116ec699604f4f13189e86a34112bf34a5713
Arturo <sergioarturoespinoza@gmail.com>
2023-10-13 14:26:13 -0600

    removing unused directories from test projects

--- core-codemods/src/test/resources/test-applications/verbose-request-mapping/build.gradle.kts
+++ core-codemods/src/test/resources/test-applications/verbose-request-mapping/build.gradle.kts
@@ -19,7 +19,6 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
