bb727f73bf701cc8ab59aaa3da530eb15b09f824
Arturo <sergioarturoespinoza@gmail.com>
2023-10-25 10:09:40 -0600

    fixing checkstyle error

--- core-codemods/src/test/java/io/codemodder/codemods/integration/tests/ReplaceDefaultHttpClientCodemodIntegrationTest.java
+++ core-codemods/src/test/java/io/codemodder/codemods/integration/tests/ReplaceDefaultHttpClientCodemodIntegrationTest.java
@@ -5,9 +5,11 @@ import io.codemodder.codemods.integration.util.IntegrationTestMetadata;
 import io.codemodder.codemods.integration.util.IntegrationTestPropertiesMetadata;
 
 @IntegrationTestMetadata(
-        codemodId = "replace-apache-defaulthttpclient",
-        tests = {
-                @IntegrationTestPropertiesMetadata(endpoint = "/test/country/mexico/capital", expectedResponse = "Mexico City")
-        })
-public class ReplaceDefaultHttpClientCodemodIntegrationTest implements CodemodIntegrationTestMixin {
-}
+    codemodId = "replace-apache-defaulthttpclient",
+    tests = {
+      @IntegrationTestPropertiesMetadata(
+          endpoint = "/test/country/mexico/capital",
+          expectedResponse = "Mexico City")
+    })
+public class ReplaceDefaultHttpClientCodemodIntegrationTest
+    implements CodemodIntegrationTestMixin {}
