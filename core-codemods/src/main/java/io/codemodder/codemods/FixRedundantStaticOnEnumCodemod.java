4dcfbffd01a3216551dbbfbe8503878ad7882f01
Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>
2023-11-21 19:31:48 -0500

    added support for static enums getting fixed

---
+++ core-codemods/src/main/java/io/codemodder/codemods/FixRedundantStaticOnEnumCodemod.java
@@ -0,0 +1,38 @@
+package io.codemodder.codemods;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.EnumDeclaration;
+import io.codemodder.*;
+import io.codemodder.providers.sonar.ProvidedSonarScan;
+import io.codemodder.providers.sonar.RuleIssues;
+import io.codemodder.providers.sonar.SonarPluginJavaParserChanger;
+import io.codemodder.providers.sonar.api.Issue;
+import javax.inject.Inject;
+
+/** A codemod for automatically removing redundant static flags on nested enums. */
+@Codemod(
+    id = "sonar:java/remove-redundant-static-s2786",
+    reviewGuidance = ReviewGuidance.MERGE_WITHOUT_REVIEW,
+    executionPriority = CodemodExecutionPriority.HIGH)
+public final class FixRedundantStaticOnEnumCodemod
+    extends SonarPluginJavaParserChanger<EnumDeclaration> {
+
+  @Inject
+  public FixRedundantStaticOnEnumCodemod(
+      @ProvidedSonarScan(ruleId = "java:S2786") final RuleIssues issues) {
+    super(issues, EnumDeclaration.class, RegionNodeMatcher.MATCHES_START);
+  }
+
+  @Override
+  public boolean onIssueFound(
+      final CodemodInvocationContext context,
+      final CompilationUnit cu,
+      final EnumDeclaration enumDecl,
+      final Issue issue) {
+    if (enumDecl.isStatic()) {
+      enumDecl.setStatic(false);
+      return true;
+    }
+    return false;
+  }
+}
