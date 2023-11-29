fe6aadce84a549f78eaf0e5b12ba5b46ba968ebf
Carlos Uscanga <carlos.07um@gmail.com>
2023-11-29 12:02:30 -0600

    spotless

--- core-codemods/src/main/java/io/codemodder/codemods/ReplaceCodemod.java
+++ core-codemods/src/main/java/io/codemodder/codemods/ReplaceCodemod.java
@@ -1,32 +1,33 @@
 package io.codemodder.codemods;
 
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.SimpleName;
 import io.codemodder.*;
 import io.codemodder.providers.sonar.ProvidedSonarScan;
 import io.codemodder.providers.sonar.RuleIssues;
 import io.codemodder.providers.sonar.SonarPluginJavaParserChanger;
 import io.codemodder.providers.sonar.api.Issue;
-
 import javax.inject.Inject;
 
-/** A codemod for automatically replacing replaceAll() calls to replace()  . */
+/** A codemod for automatically replacing replaceAll() calls to replace() . */
 @Codemod(
-        id = "sonar:java/replace-s5361",
-        reviewGuidance = ReviewGuidance.MERGE_WITHOUT_REVIEW,
-        executionPriority = CodemodExecutionPriority.HIGH)
+    id = "sonar:java/replace-s5361",
+    reviewGuidance = ReviewGuidance.MERGE_WITHOUT_REVIEW,
+    executionPriority = CodemodExecutionPriority.HIGH)
 public class ReplaceCodemod extends SonarPluginJavaParserChanger<SimpleName> {
 
-    @Inject
-    public ReplaceCodemod(
-            @ProvidedSonarScan(ruleId = "java:S5361") final RuleIssues issues) {
-        super(issues, SimpleName.class, RegionNodeMatcher.MATCHES_START);
-    }
+  @Inject
+  public ReplaceCodemod(@ProvidedSonarScan(ruleId = "java:S5361") final RuleIssues issues) {
+    super(issues, SimpleName.class, RegionNodeMatcher.MATCHES_START);
+  }
 
-    @Override
-    public boolean onIssueFound(final CodemodInvocationContext context, final CompilationUnit cu, final SimpleName name, final Issue issue) {
-        name.setIdentifier("replace");
-        return true;
-    }
+  @Override
+  public boolean onIssueFound(
+      final CodemodInvocationContext context,
+      final CompilationUnit cu,
+      final SimpleName name,
+      final Issue issue) {
+    name.setIdentifier("replace");
+    return true;
+  }
 }
--- core-codemods/src/test/java/io/codemodder/codemods/ReplaceCodemodTest.java
+++ core-codemods/src/test/java/io/codemodder/codemods/ReplaceCodemodTest.java
@@ -4,9 +4,8 @@ import io.codemodder.testutils.CodemodTestMixin;
 import io.codemodder.testutils.Metadata;
 
 @Metadata(
-        codemodType = ReplaceCodemod.class,
-        testResourceDir = "replace-s5361",
-        renameTestFile = "src/main/java/org/owasp/webgoat/lessons/ssrf/SSRFTask2.java",
-        dependencies = {})
-public class ReplaceCodemodTest implements CodemodTestMixin {
-}
+    codemodType = ReplaceCodemod.class,
+    testResourceDir = "replace-s5361",
+    renameTestFile = "src/main/java/org/owasp/webgoat/lessons/ssrf/SSRFTask2.java",
+    dependencies = {})
+public class ReplaceCodemodTest implements CodemodTestMixin {}
