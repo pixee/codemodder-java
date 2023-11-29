1ab67928ef79769bb5e55706f52d698090de0e1c
Carlos Uscanga <carlos.07um@gmail.com>
2023-11-29 12:14:24 -0600

    refactor

--- core-codemods/src/test/java/io/codemodder/codemods/AddMissingOverrideCodemodTest.java
+++ core-codemods/src/test/java/io/codemodder/codemods/AddMissingOverrideCodemodTest.java
@@ -6,5 +6,6 @@ import io.codemodder.testutils.Metadata;
 @Metadata(
     codemodType = AddMissingOverrideCodemod.class,
     testResourceDir = "add-missing-override-s1161",
+    renameTestFile = "src/main/java/SqlInjectionLesson10b.java",
     dependencies = {})
 final class AddMissingOverrideCodemodTest implements CodemodTestMixin {}
