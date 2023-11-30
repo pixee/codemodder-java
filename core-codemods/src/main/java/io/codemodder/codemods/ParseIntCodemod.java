bcc1ac573c8d8b1b3ca83bae6ddd84d34644fa7c
Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>
2023-11-30 16:07:29 -0500

    fixed sonar missing some injectable parameters

--- core-codemods/src/main/java/io/codemodder/codemods/ParseIntCodemod.java
+++ core-codemods/src/main/java/io/codemodder/codemods/ParseIntCodemod.java
@@ -1,50 +1,44 @@
 package io.codemodder.codemods;
 
-import com.contrastsecurity.sarif.Result;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.expr.ObjectCreationExpr;
 import io.codemodder.*;
-import io.codemodder.providers.sarif.semgrep.SemgrepScan;
 import io.codemodder.providers.sonar.ProvidedSonarScan;
 import io.codemodder.providers.sonar.RuleIssues;
 import io.codemodder.providers.sonar.SonarPluginJavaParserChanger;
 import io.codemodder.providers.sonar.api.Issue;
 import javax.inject.Inject;
-import java.util.List;
-
-import static io.codemodder.javaparser.JavaParserTransformer.replace;
 
 /** A codemod for automatically using the relevant integer parsing method . */
 @Codemod(
     id = "sonar:java/parse-int-s2130",
     reviewGuidance = ReviewGuidance.MERGE_WITHOUT_REVIEW,
     executionPriority = CodemodExecutionPriority.HIGH)
-public final class ParseIntCodemod extends CompositeJavaParserChanger{
+public final class ParseIntCodemod extends CompositeJavaParserChanger {
+
+  @Inject
+  public ParseIntCodemod(
+      final ParseIntCodemod.ParseMethodCallExprChanger parseMethodCallExprChanger) {
+    super(parseMethodCallExprChanger);
+  }
+
+  private static class ParseMethodCallExprChanger
+      extends SonarPluginJavaParserChanger<MethodCallExpr> {
 
     @Inject
-    public ParseIntCodemod(
-            final ParseIntCodemod.ParseMethodCallExprChanger parseMethodCallExprChanger) {
-        super(parseMethodCallExprChanger);
+    public ParseMethodCallExprChanger(
+        @ProvidedSonarScan(ruleId = "java:S2130") final RuleIssues issues) {
+      super(issues, MethodCallExpr.class, RegionNodeMatcher.MATCHES_START);
     }
 
-    private static class ParseMethodCallExprChanger
-            extends SonarPluginJavaParserChanger<MethodCallExpr> {
-
-        @Inject
-        public ParseMethodCallExprChanger(@ProvidedSonarScan(ruleId = "java:S2130") final RuleIssues issues) {
-            super(issues, MethodCallExpr.class, RegionNodeMatcher.MATCHES_START);
-        }
-
-        @Override
-        public boolean onIssueFound(
-                final CodemodInvocationContext context,
-                final CompilationUnit cu,
-                final MethodCallExpr methodCallExpr,
-                final Issue issue) {
-            methodCallExpr.setName("parseInt");
-            return true;
-        }
+    @Override
+    public boolean onIssueFound(
+        final CodemodInvocationContext context,
+        final CompilationUnit cu,
+        final MethodCallExpr methodCallExpr,
+        final Issue issue) {
+      methodCallExpr.setName("parseInt");
+      return true;
     }
+  }
 }
