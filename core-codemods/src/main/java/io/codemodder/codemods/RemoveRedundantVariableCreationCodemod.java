70d030d05152ee6ea011a8e0181096ea49cf1cc6
Carlos Uscanga <carlos.07um@gmail.com>
2023-12-04 15:29:14 -0600

    working

---
+++ core-codemods/src/main/java/io/codemodder/codemods/RemoveRedundantVariableCreationCodemod.java
@@ -0,0 +1,76 @@
+package io.codemodder.codemods;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.expr.*;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ExpressionStmt;
+import com.github.javaparser.ast.stmt.ReturnStmt;
+import io.codemodder.*;
+import io.codemodder.providers.sonar.ProvidedSonarScan;
+import io.codemodder.providers.sonar.RuleIssues;
+import io.codemodder.providers.sonar.SonarPluginJavaParserChanger;
+import io.codemodder.providers.sonar.api.Issue;
+import java.util.Optional;
+import javax.inject.Inject;
+
+/** A codemod to remove redundant variable creation */
+@Codemod(
+    id = "sonar:java/remove-redundant-variable-creation-s1488",
+    reviewGuidance = ReviewGuidance.MERGE_WITHOUT_REVIEW,
+    executionPriority = CodemodExecutionPriority.HIGH)
+public final class RemoveRedundantVariableCreationCodemod extends CompositeJavaParserChanger {
+
+  @Inject
+  public RemoveRedundantVariableCreationCodemod(
+      final RemoveRedundantVariableChanger removeVariableReturnChanger) {
+    super(removeVariableReturnChanger);
+  }
+
+  private static final class RemoveRedundantVariableChanger
+      extends SonarPluginJavaParserChanger<ObjectCreationExpr> {
+
+    @Inject
+    public RemoveRedundantVariableChanger(
+        @ProvidedSonarScan(ruleId = "java:S1488") final RuleIssues issues) {
+      super(
+          issues,
+          ObjectCreationExpr.class,
+          RegionNodeMatcher.MATCHES_START,
+          CodemodReporterStrategy.empty());
+    }
+
+    @Override
+    public boolean onIssueFound(
+        final CodemodInvocationContext context,
+        final CompilationUnit cu,
+        final ObjectCreationExpr objectCreationExpr,
+        final Issue issue) {
+
+      // Get full block statement
+      final Optional<BlockStmt> blockStmtOpt = objectCreationExpr.findAncestor(BlockStmt.class);
+      if (blockStmtOpt.isPresent()) {
+        final BlockStmt blockStmt = blockStmtOpt.get();
+
+        // Retrieve return statement to update its expression to objectCreationExpr param
+        final Optional<ReturnStmt> returnStmtOpt =
+            blockStmt.getStatements().stream()
+                .filter(ReturnStmt.class::isInstance)
+                .map(ReturnStmt.class::cast)
+                .findFirst();
+
+        returnStmtOpt.ifPresent(
+            returnStmt -> {
+              final Expression expression = objectCreationExpr.clone();
+              returnStmt.setExpression(expression);
+
+              // Remove the redundant variable creation expression
+              final Optional<ExpressionStmt> exprStmtOpt =
+                  objectCreationExpr.findAncestor(ExpressionStmt.class);
+              exprStmtOpt.ifPresent(exprStmt -> blockStmt.getStatements().remove(exprStmt));
+            });
+      }
+
+      return true;
+    }
+  }
+}
--- core-codemods/src/test/java/io/codemodder/codemods/RemoveRedundantVariableCreationCodemodTest.java
+++ core-codemods/src/test/java/io/codemodder/codemods/RemoveRedundantVariableCreationCodemodTest.java
@@ -4,8 +4,8 @@ import io.codemodder.testutils.CodemodTestMixin;
 import io.codemodder.testutils.Metadata;
 
 @Metadata(
-    codemodType = RemoveRedundantVariableDeclarationCodemod.class,
-    testResourceDir = "remove-redundant-variable-declaration-s1488",
+    codemodType = RemoveRedundantVariableCreationCodemod.class,
+    testResourceDir = "remove-redundant-variable-creation-s1488",
     renameTestFile = "src/main/java/org/owasp/webgoat/container/MvcConfiguration.java",
     dependencies = {})
-final class RemoveRedundantVariableDeclarationCodemodTest implements CodemodTestMixin {}
+final class RemoveRedundantVariableCreationCodemodTest implements CodemodTestMixin {}
