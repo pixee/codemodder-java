6b9127f01c35d9a3cc5963d6a7e4927f716047c8
Carlos Uscanga <carlos.07um@gmail.com>
2023-12-06 15:09:09 -0600

    done

--- core-codemods/src/main/java/io/codemodder/codemods/AvoidImplicitPublicConstructorCodemod.java
+++ core-codemods/src/main/java/io/codemodder/codemods/AvoidImplicitPublicConstructorCodemod.java
@@ -1,12 +1,19 @@
 package io.codemodder.codemods;
 
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
+import com.github.javaparser.ast.expr.SimpleName;
 import io.codemodder.*;
 import io.codemodder.providers.sonar.ProvidedSonarScan;
 import io.codemodder.providers.sonar.RuleIssues;
 import io.codemodder.providers.sonar.SonarPluginJavaParserChanger;
 import io.codemodder.providers.sonar.api.Issue;
+import java.util.Optional;
 import javax.inject.Inject;
 
 /** A codemod for setting a private constructor to hide implicit public constructor (Sonar) */
@@ -15,20 +22,38 @@ import javax.inject.Inject;
     reviewGuidance = ReviewGuidance.MERGE_WITHOUT_REVIEW,
     executionPriority = CodemodExecutionPriority.HIGH)
 public final class AvoidImplicitPublicConstructorCodemod
-    extends SonarPluginJavaParserChanger<Node> {
+    extends SonarPluginJavaParserChanger<SimpleName> {
 
   @Inject
   public AvoidImplicitPublicConstructorCodemod(
       @ProvidedSonarScan(ruleId = "java:S1118") final RuleIssues issues) {
-    super(issues, Node.class, RegionNodeMatcher.MATCHES_START);
+    super(issues, SimpleName.class, RegionNodeMatcher.MATCHES_START);
   }
 
   @Override
   public boolean onIssueFound(
       final CodemodInvocationContext context,
       final CompilationUnit cu,
-      final Node node,
+      final SimpleName simpleName,
       final Issue issue) {
+
+    final Optional<Node> classOptional = simpleName.getParentNode();
+
+    if (classOptional.isPresent()
+        && classOptional.get() instanceof ClassOrInterfaceDeclaration classNode) {
+      // Create a constructor
+      final ConstructorDeclaration constructor = new ConstructorDeclaration();
+      constructor.setName(classNode.getName());
+      constructor.setModifiers(Modifier.privateModifier().getKeyword());
+
+      // Add the constructor at the beginning of the class node's members
+      NodeList<BodyDeclaration<?>> members = classNode.getMembers();
+      members.add(0, constructor);
+
+      // Update the class node's members
+      classNode.setMembers(members);
+      return true; // Return true if the modification was successful
+    }
 
     return false;
   }
