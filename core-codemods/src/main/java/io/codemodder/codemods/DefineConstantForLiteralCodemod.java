2e72bbf42ba33291ff7d66d2bd8938439dcfe2d1
Carlos Uscanga <carlos.07um@gmail.com>
2023-12-13 16:08:16 -0600

    define constant done

--- core-codemods/src/main/java/io/codemodder/codemods/DefineConstantForLiteralCodemod.java
+++ core-codemods/src/main/java/io/codemodder/codemods/DefineConstantForLiteralCodemod.java
@@ -1,10 +1,15 @@
 package io.codemodder.codemods;
 
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.Node;
-import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.expr.StringLiteralExpr;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.Type;
 import io.codemodder.*;
 import io.codemodder.providers.sonar.ProvidedSonarScan;
 import io.codemodder.providers.sonar.RuleIssues;
@@ -34,6 +39,46 @@ public final class DefineConstantForLiteralCodemod extends SonarPluginJavaParser
       final StringLiteralExpr stringLiteralExpr,
       final Issue issue) {
 
-    return false;
+
+        if(defineConstant(stringLiteralExpr)){
+
+            return true;
+        }
+
+        return false;
+  }
+
+  private boolean defineConstant(
+          final StringLiteralExpr stringLiteralExpr){
+      final Optional<ClassOrInterfaceDeclaration> classOrInterfaceDeclarationOptional = stringLiteralExpr.findAncestor(ClassOrInterfaceDeclaration.class);
+
+      if(classOrInterfaceDeclarationOptional.isPresent()){
+          final String constantName = stringLiteralExpr.getValue().toUpperCase().concat("_CONSTANT");
+
+          NodeList<Modifier> modifiers = NodeList.nodeList(Modifier.privateModifier(), Modifier.staticModifier(), Modifier.finalModifier());
+
+          Type type = new ClassOrInterfaceType(null, "String");
+
+          // Creating the VariableDeclarationExpr
+          VariableDeclarator variableDeclarator = new VariableDeclarator();
+          variableDeclarator.setInitializer(new StringLiteralExpr(stringLiteralExpr.getValue()));
+          variableDeclarator.setType(type);
+          variableDeclarator.setName(constantName);
+
+
+          // Creating the FieldDeclaration
+          FieldDeclaration fieldDeclaration = new FieldDeclaration(modifiers, variableDeclarator);
+
+          final ClassOrInterfaceDeclaration classOrInterfaceDeclaration = classOrInterfaceDeclarationOptional.get();
+          NodeList<BodyDeclaration<?>> members = classOrInterfaceDeclaration.getMembers();
+
+          // Add field declaration to the top of the members of the class
+          members.addFirst(fieldDeclaration);
+
+          return true;
+
+      }
+
+      return false;
   }
 }
