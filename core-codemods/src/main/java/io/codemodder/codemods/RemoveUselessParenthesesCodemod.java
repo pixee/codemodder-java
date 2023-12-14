a2c4cfa84e36553a45289eebb16e74085a0e2c58
Carlos Uscanga <carlos.07um@gmail.com>
2023-12-14 12:47:51 -0600

    done ??

---
+++ core-codemods/src/main/java/io/codemodder/codemods/RemoveUselessParenthesesCodemod.java
@@ -0,0 +1,42 @@
+package io.codemodder.codemods;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.expr.EnclosedExpr;
+import com.github.javaparser.ast.expr.Expression;
+import io.codemodder.Codemod;
+import io.codemodder.CodemodExecutionPriority;
+import io.codemodder.CodemodInvocationContext;
+import io.codemodder.ReviewGuidance;
+import io.codemodder.providers.sonar.ProvidedSonarScan;
+import io.codemodder.providers.sonar.RuleIssues;
+import io.codemodder.providers.sonar.SonarPluginJavaParserChanger;
+import io.codemodder.providers.sonar.api.Issue;
+import javax.inject.Inject;
+
+/** Codemod to remove useless pair of parentheses */
+@Codemod(
+    id = "sonar:java/remove-useless-parentheses-s1110",
+    reviewGuidance = ReviewGuidance.MERGE_WITHOUT_REVIEW,
+    executionPriority = CodemodExecutionPriority.HIGH)
+public final class RemoveUselessParenthesesCodemod
+    extends SonarPluginJavaParserChanger<EnclosedExpr> {
+
+  @Inject
+  public RemoveUselessParenthesesCodemod(
+      @ProvidedSonarScan(ruleId = "java:S1110") final RuleIssues issues) {
+    super(issues, EnclosedExpr.class);
+  }
+
+  @Override
+  public boolean onIssueFound(
+      final CodemodInvocationContext context,
+      final CompilationUnit cu,
+      final EnclosedExpr enclosedExpr,
+      final Issue issue) {
+
+    Expression innerExpr = enclosedExpr.getInner();
+    enclosedExpr.replace(innerExpr);
+
+    return true;
+  }
+}
--- core-codemods/src/main/resources/io/codemodder/codemods/RemoveUselessParenthesesCodemod/description.md
+++ core-codemods/src/main/resources/io/codemodder/codemods/RemoveUselessParenthesesCodemod/description.md
@@ -1,6 +1,10 @@
+TODO
 
 Our changes look something like this:
 
 ```diff
+
+-     int leftOver = (int) (((bitCount >>> 3)) & 0x3f);
++     int leftOver = (int) ((bitCount >>> 3) & 0x3f);
 
 ```
