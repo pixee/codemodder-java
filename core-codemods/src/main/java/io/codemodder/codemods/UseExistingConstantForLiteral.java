d5bdd8896c6de6e068571136d25e9fc9779fd0e8
Carlos Uscanga <carlos.07um@gmail.com>
2024-01-11 08:42:46 -0600

    javadoc

--- core-codemods/src/main/java/io/codemodder/codemods/UseExistingConstantForLiteral.java
+++ core-codemods/src/main/java/io/codemodder/codemods/UseExistingConstantForLiteral.java
@@ -15,20 +15,22 @@ public class UseExistingConstantForLiteral extends DefineConstantForLiteral {
     super(context, cu, stringLiteralExpr, issue);
   }
 
+  /**
+   * Retrieves the suggested constant name from the issue's message. The issue's message is expected
+   * to follow a format such as "Use already-defined constant 'MY_CONSTANT' instead of duplicating
+   * its value here." The method extracts the constant name found between the single quotes ('').
+   */
   @Override
   protected String getConstantName() {
-    return getDefinedConstantValueInMessage(issue.getMessage());
+    final String message = issue.getMessage();
+    int startIndex = message.indexOf("'");
+    int endIndex = message.lastIndexOf("'");
+
+    return message.substring(startIndex + 1, endIndex);
   }
 
   @Override
   protected void defineConstant(final String constantName) {
     // Empty implementation
-  }
-
-  private String getDefinedConstantValueInMessage(final String message) {
-    int startIndex = message.indexOf("'");
-    int endIndex = message.lastIndexOf("'");
-
-    return message.substring(startIndex + 1, endIndex);
   }
 }
