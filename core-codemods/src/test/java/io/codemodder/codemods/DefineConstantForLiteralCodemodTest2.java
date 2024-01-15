e94b19371f6c7e6a215ae7889b9ba590aaa3bf73
Carlos Uscanga <carlos.07um@gmail.com>
2024-01-15 15:46:37 -0600

    remove comments

--- core-codemods/src/test/java/io/codemodder/codemods/DefineConstantForLiteralCodemodTest2.java
+++ core-codemods/src/test/java/io/codemodder/codemods/DefineConstantForLiteralCodemodTest2.java
@@ -3,11 +3,7 @@ package io.codemodder.codemods;
 import io.codemodder.testutils.CodemodTestMixin;
 import io.codemodder.testutils.Metadata;
 
-/**
- * This test a case where we expect to: 1. Create a new camel case constant taking other constants
- * naming convention as reference 2. Avoid expected collision by adding a counter suffix to new
- * constant
- */
+
 @Metadata(
     codemodType = DefineConstantForLiteralCodemod.class,
     testResourceDir = "define-constant-for-duplicate-literal-s1192/case-2",
