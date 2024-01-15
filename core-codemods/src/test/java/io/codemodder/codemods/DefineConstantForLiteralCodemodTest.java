9af23f02c737eb119854c7e4bcefe911716773ce
Carlos Uscanga <carlos.07um@gmail.com>
2024-01-15 15:47:29 -0600

    spotless

--- core-codemods/src/test/java/io/codemodder/codemods/DefineConstantForLiteralCodemodTest.java
+++ core-codemods/src/test/java/io/codemodder/codemods/DefineConstantForLiteralCodemodTest.java
@@ -3,7 +3,6 @@ package io.codemodder.codemods;
 import io.codemodder.testutils.CodemodTestMixin;
 import io.codemodder.testutils.Metadata;
 
-
 @Metadata(
     codemodType = DefineConstantForLiteralCodemod.class,
     testResourceDir = "define-constant-for-duplicate-literal-s1192/case-1",
