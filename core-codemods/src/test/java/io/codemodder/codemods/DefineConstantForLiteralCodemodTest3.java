3e9f2f1948c7c36fc4e8e1f15742d99e55b54964
Carlos Uscanga <carlos.07um@gmail.com>
2024-01-15 13:02:00 -0600

    aws test

---
+++ core-codemods/src/test/java/io/codemodder/codemods/DefineConstantForLiteralCodemodTest3.java
@@ -0,0 +1,10 @@
+package io.codemodder.codemods;
+
+import io.codemodder.testutils.CodemodTestMixin;
+import io.codemodder.testutils.Metadata;
+@Metadata(
+    codemodType = DefineConstantForLiteralCodemod.class,
+    testResourceDir = "define-constant-for-duplicate-literal-s1192-3",
+    renameTestFile = "codegen-lite/src/main/java/software/amazon/awssdk/codegen/lite/regions/PartitionMetadataGenerator.java",
+    dependencies = {})
+final class DefineConstantForLiteralCodemodTest3 implements CodemodTestMixin {}
