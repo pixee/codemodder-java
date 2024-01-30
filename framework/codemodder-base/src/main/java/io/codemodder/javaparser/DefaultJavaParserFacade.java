92f42acde5e54a98b617fc3ac4cfde580781e562
Ryan Dens <ryan.dens@pixee.ai>
2024-01-30 08:20:04 -0800

    :loud_sound: log parsing problems to help debugging issues in production

--- framework/codemodder-base/src/main/java/io/codemodder/javaparser/DefaultJavaParserFacade.java
+++ framework/codemodder-base/src/main/java/io/codemodder/javaparser/DefaultJavaParserFacade.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Objects;
 import javax.inject.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 final class DefaultJavaParserFacade implements JavaParserFacade {
 
@@ -29,10 +31,14 @@ final class DefaultJavaParserFacade implements JavaParserFacade {
 
     final ParseResult<CompilationUnit> result = parser.parse(file);
     if (!result.isSuccessful()) {
+      logger.error(
+          "Error while parsing file {} encountered problems: {}", file, result.getProblems());
       throw new RuntimeException("can't parse file");
     }
     CompilationUnit cu = result.getResult().orElseThrow();
     LexicalPreservingPrinter.setup(cu);
     return cu;
   }
+
+  private static final Logger logger = LoggerFactory.getLogger(DefaultJavaParserFacade.class);
 }
