ece022bbcf71a93dcd23af82485c2811215b93cd
Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>
2024-03-29 09:28:04 -0400

    nitpick

--- framework/codemodder-base/src/main/java/io/codemodder/javaparser/DefaultChangesResult.java
+++ framework/codemodder-base/src/main/java/io/codemodder/javaparser/DefaultChangesResult.java
@@ -2,15 +2,17 @@ package io.codemodder.javaparser;
 
 import io.codemodder.DependencyGAV;
 import java.util.List;
+import java.util.Objects;
 
-class DefaultChangesResult implements ChangesResult {
+final class DefaultChangesResult implements ChangesResult {
+  
   private final boolean changesApplied;
   private final List<DependencyGAV> dependenciesRequired;
 
   DefaultChangesResult(
       final boolean changesApplied, final List<DependencyGAV> dependenciesRequired) {
     this.changesApplied = changesApplied;
-    this.dependenciesRequired = dependenciesRequired;
+    this.dependenciesRequired = Objects.requireNonNull(dependenciesRequired);
   }
 
   @Override
