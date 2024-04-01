4a9307b4fb8010dcc430cb3191554cd4182a0190
andrecs <12188364+andrecsilva@users.noreply.github.com>
2024-04-01 14:31:35 -0300

    fixup! Added some tests

--- framework/codemodder-base/src/test/java/io/codemodder/ast/RemoveEmptyStringConcatenationTest.java
+++ framework/codemodder-base/src/test/java/io/codemodder/ast/RemoveEmptyStringConcatenationTest.java
@@ -1,18 +1,20 @@
 package io.codemodder.ast;
 
-import static io.codemodder.ast.TestUtils.parseCode;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.VariableDeclarator;
+import io.codemodder.javaparser.JavaParserFactory;
+import java.io.IOException;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 final class RemoveEmptyStringConcatenationTest {
 
   @Test
-  void it_removes_right() {
+  void it_removes_right() throws IOException {
     String code =
         """
 	    class A{
@@ -21,7 +23,8 @@ final class RemoveEmptyStringConcatenationTest {
 		    }
 	    }
     """;
-    CompilationUnit cu = parseCode(code);
+    CompilationUnit cu =
+        JavaParserFactory.newFactory().create(List.of()).parse(code).getResult().get();
 
     var methodDecl = cu.findAll(MethodDeclaration.class).get(0);
     var node = cu.findAll(VariableDeclarator.class).get(0);
@@ -31,7 +34,8 @@ final class RemoveEmptyStringConcatenationTest {
     assertThat(node.getInitializer().get().toString(), equalTo("\"first\""));
   }
 
-  void it_removes_left() {
+  @Test
+  void it_removes_left() throws IOException {
     String code =
         """
 	    class A{
@@ -40,7 +44,8 @@ final class RemoveEmptyStringConcatenationTest {
 		    }
 	    }
     """;
-    CompilationUnit cu = parseCode(code);
+    CompilationUnit cu =
+        JavaParserFactory.newFactory().create(List.of()).parse(code).getResult().get();
     var methodDecl = cu.findAll(MethodDeclaration.class).get(0);
     var node = cu.findAll(VariableDeclarator.class).get(0);
 
@@ -49,7 +54,8 @@ final class RemoveEmptyStringConcatenationTest {
     assertThat(node.getInitializer().get().toString(), equalTo("\"second\""));
   }
 
-  void it_removes_concatenation() {
+  @Test
+  void it_removes_concatenation() throws IOException {
     String code =
         """
 	    class A{
@@ -58,7 +64,8 @@ final class RemoveEmptyStringConcatenationTest {
 		    }
 	    }
     """;
-    CompilationUnit cu = parseCode(code);
+    CompilationUnit cu =
+        JavaParserFactory.newFactory().create(List.of()).parse(code).getResult().get();
     var methodDecl = cu.findAll(MethodDeclaration.class).get(0);
     var node = cu.findAll(VariableDeclarator.class).get(0);
 
@@ -67,7 +74,8 @@ final class RemoveEmptyStringConcatenationTest {
     assertThat(node.getInitializer().get().toString(), equalTo("\"\""));
   }
 
-  void it_resolves_and_removes_left() {
+  @Test
+  void it_resolves_and_removes_left() throws IOException {
     String code =
         """
 	    class A{
@@ -77,7 +85,8 @@ final class RemoveEmptyStringConcatenationTest {
 		    }
 	    }
     """;
-    CompilationUnit cu = parseCode(code);
+    CompilationUnit cu =
+        JavaParserFactory.newFactory().create(List.of()).parse(code).getResult().get();
     var methodDecl = cu.findAll(MethodDeclaration.class).get(0);
     var node = cu.findAll(VariableDeclarator.class).get(1);
 
--- framework/codemodder-base/src/test/java/io/codemodder/ast/RemoveUnusedLocalVariablesTest.java
+++ framework/codemodder-base/src/test/java/io/codemodder/ast/RemoveUnusedLocalVariablesTest.java
@@ -1,6 +1,5 @@
 package io.codemodder.ast;
 
-import static io.codemodder.ast.TestUtils.parseCode;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -8,12 +7,15 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.expr.AssignExpr;
+import io.codemodder.javaparser.JavaParserFactory;
+import java.io.IOException;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 final class RemoveUnusedLocalVariablesTest {
 
   @Test
-  void it_removes_s() {
+  void it_removes_s() throws IOException {
     String code =
         """
 	    class A{
@@ -22,7 +24,8 @@ final class RemoveUnusedLocalVariablesTest {
 		    }
 	    }
     """;
-    CompilationUnit cu = parseCode(code);
+    CompilationUnit cu =
+        JavaParserFactory.newFactory().create(List.of()).parse(code).getResult().get();
 
     var methodDecl = cu.findAll(MethodDeclaration.class).get(0);
 
@@ -32,7 +35,7 @@ final class RemoveUnusedLocalVariablesTest {
   }
 
   @Test
-  void it_removes_s_definite_assignment() {
+  void it_removes_s_definite_assignment() throws IOException {
     String code =
         """
 	    class A{
@@ -42,7 +45,8 @@ final class RemoveUnusedLocalVariablesTest {
 		    }
 	    }
     """;
-    CompilationUnit cu = parseCode(code);
+    CompilationUnit cu =
+        JavaParserFactory.newFactory().create(List.of()).parse(code).getResult().get();
 
     var methodDecl = cu.findAll(MethodDeclaration.class).get(0);
 
