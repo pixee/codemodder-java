659832e3597067c6906e35eb1961412f2a4f2308
André C. Silva <12188364+andrecsilva@users.noreply.github.com>
2024-04-02 09:55:19 -0300

    Adds remove empty string concatenation transform after sql parameterizer transform call in test (#326)
    The removal of empty strings after parameterization by
    `SQLParameterizer` is now done as a separate transform in
    `ASTs.RemoveEmptyStringConcatenation`. This PR adds this transformation
    after the parameterization in the newly added test.

--- core-codemods/src/main/java/io/codemodder/codemods/DefectDojoSqlInjectionCodemod.java
+++ core-codemods/src/main/java/io/codemodder/codemods/DefectDojoSqlInjectionCodemod.java
@@ -1,8 +1,10 @@
 package io.codemodder.codemods;
 
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.CallableDeclaration;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import io.codemodder.*;
+import io.codemodder.ast.ASTTransforms;
 import io.codemodder.codetf.DetectionTool;
 import io.codemodder.codetf.DetectorFinding;
 import io.codemodder.codetf.DetectorRule;
@@ -92,6 +94,13 @@ public final class DefectDojoSqlInjectionCodemod extends JavaParserChanger
       SQLParameterizer parameterizer = new SQLParameterizer(methodCallExpr);
 
       if (parameterizer.checkAndFix()) {
+        final var maybeMethodDecl = methodCallExpr.findAncestor(CallableDeclaration.class);
+        // Cleanup, removes empty string concatenations and unused variables
+        maybeMethodDecl.ifPresent(cd -> ASTTransforms.removeEmptyStringConcatenation(cd));
+        // TODO hits a bug with javaparser, where adding nodes won't result in the correct children
+        // order. This causes the following to remove actually used variables
+        // maybeMethodDecl.ifPresent(md -> ASTTransforms.removeUnusedLocalVariables(md));
+
         DetectorFinding fixedFinding = new DetectorFinding(id, true, null);
         allFindings.add(fixedFinding);
         changes.add(CodemodChange.from(line, "Fixes issue " + id + " by parameterizing SQL"));
