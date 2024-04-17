297281dbf509d92ea3818f8b8d2cbe642069850d
Carlos Uscanga <carlos.07um@gmail.com>
2024-04-17 13:16:55 -0600

    remove finding factory

--- framework/codemodder-base/src/main/java/io/codemodder/FindingFactory.java
+++
@@ -1,9 +0,0 @@
-package io.codemodder;
-
-import io.codemodder.codetf.DetectorRule;
-import io.codemodder.codetf.FixedFinding;
-
-public interface FindingFactory {
-
-  FixedFinding buildFixedFinding(String id, DetectorRule detectorRule);
-}
