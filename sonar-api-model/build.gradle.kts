e74e7fd921c59e5d6c6458eaf49906605710284d
Carlos Uscanga <carlos.07um@gmail.com>
2024-06-11 11:21:59 -0600

    update

--- sonar-api-model/build.gradle.kts
+++ sonar-api-model/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
-    id("java-library")
-    id("maven-publish")
+    id("io.codemodder.java-library")
 }
 
 dependencies {
     api(libs.jackson.yaml)
     api(libs.jackson.core)
 }
+
+description = "Library with API models for data from Sonar tools"
