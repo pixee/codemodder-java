e9da3635526f101c7461ce8e0119a035ddc65b24
Carlos Uscanga <carlos.07um@gmail.com>
2024-06-11 12:17:23 -0600

    Revert "fix"
    This reverts commit aee06f905ed2c4499c0d2a5968177f8b2bee6e19.

--- plugins/codemodder-plugin-sonar/src/main/java/io/codemodder/providers/sonar/SonarModule.java
+++ plugins/codemodder-plugin-sonar/src/main/java/io/codemodder/providers/sonar/SonarModule.java
@@ -37,7 +37,24 @@ final class SonarModule extends AbstractModule {
 
   @Override
   protected void configure() {
+    configureIssues();
+  }
 
+  private void configureIssues() {
+    final List<Issue> allIssues = getIssues();
+    Map<String, List<Issue>> issuesByRuleMap = groupIssuesByRule(allIssues);
+
+    Set<String> packagesScanned = new HashSet<>();
+    for (final Class<? extends CodeChanger> codemodType : codemodTypes) {
+      String packageName = codemodType.getPackageName();
+      if (!packagesScanned.contains(packageName)) {
+        packagesScanned.add(packageName);
+        bindAnnotationsForPackage(packageName, issuesByRuleMap);
+      }
+    }
+  }
+
+  private List<Issue> getIssues() {
     List<Issue> allIssues = List.of();
     if (issuesFiles != null) {
       List<SearchIssueResponse> issueResponses = new ArrayList<>();
@@ -59,88 +76,100 @@ final class SonarModule extends AbstractModule {
           issueResponses.stream().flatMap(response -> response.getIssues().stream()).toList();
     }
 
-    Map<String, List<Issue>> perRuleBreakdown = new HashMap<>();
+    return allIssues;
+  }
+
+  private Map<String, List<Issue>> groupIssuesByRule(List<Issue> allIssues) {
+    Map<String, List<Issue>> issuesByRuleMap = new HashMap<>();
     allIssues.forEach(
         issue -> {
           String rule = issue.getRule();
-          List<Issue> perRuleList =
-              perRuleBreakdown.computeIfAbsent(rule, (k) -> new ArrayList<>());
+          List<Issue> perRuleList = issuesByRuleMap.computeIfAbsent(rule, k -> new ArrayList<>());
           perRuleList.add(issue);
         });
+    return issuesByRuleMap;
+  }
 
-    // find all the  annotations and bind them as is
-    Set<String> packagesScanned = new HashSet<>();
-    for (final Class<? extends CodeChanger> codemodType : codemodTypes) {
-      String packageName = codemodType.getPackageName();
-      if (!packagesScanned.contains(packageName)) {
-        packagesScanned.add(packageName);
-        try (ScanResult scan =
-            new ClassGraph()
-                .enableAllInfo()
-                .acceptPackagesNonRecursive(packageName)
-                .removeTemporaryFilesAfterScan()
-                .scan()) {
-          ClassInfoList classesWithMethodAnnotation =
-              scan.getClassesWithMethodAnnotation(Inject.class);
-          List<Class<?>> injectableClasses = classesWithMethodAnnotation.loadClasses();
-          List<Parameter> injectableParams =
-              injectableClasses.stream()
-                  .map(Class::getDeclaredConstructors)
-                  .flatMap(Arrays::stream)
-                  .filter(constructor -> constructor.isAnnotationPresent(Inject.class))
-                  .map(Executable::getParameters)
-                  .flatMap(Arrays::stream)
-                  .toList();
+  private void bindAnnotationsForPackage(
+      String packageName, Map<String, List<Issue>> issuesByRuleMap) {
+    try (ScanResult scan =
+        new ClassGraph()
+            .enableAllInfo()
+            .acceptPackagesNonRecursive(packageName)
+            .removeTemporaryFilesAfterScan()
+            .scan()) {
 
-          // Outside the loop, create a map to track already bound ruleIds
-          final Map<String, Boolean> boundRuleIds = new HashMap<>();
+      List<Parameter> injectableParams = getInjectableParameters(scan);
 
-          injectableParams.forEach(
-              param -> {
-                ProvidedSonarScan annotation = param.getAnnotation(ProvidedSonarScan.class);
-                if (annotation == null) {
-                  return;
-                }
-                if (!RuleIssues.class.equals(param.getType())) {
-                  throw new IllegalArgumentException(
-                      "can't use @ProvidedSonarScan on anything except RuleIssues (see "
-                          + param.getDeclaringExecutable().getDeclaringClass().getName()
-                          + ")");
-                }
+      // Outside the loop, create a map to track already bound ruleIds
+      final Map<String, Boolean> boundRuleIds = new HashMap<>();
 
-                final String ruleId = annotation.ruleId();
-
-                // Check if the ruleId has already been bound
-                if (boundRuleIds.containsKey(ruleId)) {
-                  return; // Skip binding if already bound
-                }
-
-                // bind from existing scan
-                List<Issue> issues = perRuleBreakdown.get(annotation.ruleId());
-
-                if (issues == null || issues.isEmpty()) {
-                  bind(RuleIssues.class).annotatedWith(annotation).toInstance(EMPTY);
-                } else {
-                  Map<String, List<Issue>> issuesByPath = new HashMap<>();
-                  issues.forEach(
-                      issue -> {
-                        Optional<String> filename = issue.componentFileName();
-                        if (filename.isPresent()) {
-                          String fullPath = repository.resolve(filename.get()).toString();
-                          List<Issue> pathIssues =
-                              issuesByPath.computeIfAbsent(fullPath, (f) -> new ArrayList<>());
-                          pathIssues.add(issue);
-                        }
-                      });
-                  RuleIssues ruleIssues = new DefaultRuleIssues(issuesByPath);
-                  bind(RuleIssues.class).annotatedWith(annotation).toInstance(ruleIssues);
-                  // Mark the ruleId as bound
-                  boundRuleIds.put(ruleId, true);
-                }
-              });
-        }
-      }
+      injectableParams.forEach(
+          param -> {
+            ProvidedSonarScan annotation = param.getAnnotation(ProvidedSonarScan.class);
+            if (annotation != null) {
+              bindParam(annotation, param, issuesByRuleMap, boundRuleIds);
+            }
+          });
     }
+  }
+
+  private List<Parameter> getInjectableParameters(ScanResult scan) {
+    ClassInfoList classesWithMethodAnnotation = scan.getClassesWithMethodAnnotation(Inject.class);
+    List<Class<?>> injectableClasses = classesWithMethodAnnotation.loadClasses();
+    return injectableClasses.stream()
+        .map(Class::getDeclaredConstructors)
+        .flatMap(Arrays::stream)
+        .filter(constructor -> constructor.isAnnotationPresent(Inject.class))
+        .map(Executable::getParameters)
+        .flatMap(Arrays::stream)
+        .toList();
+  }
+
+  private void bindParam(
+      ProvidedSonarScan annotation,
+      Parameter param,
+      Map<String, List<Issue>> issuesByRuleMap,
+      Map<String, Boolean> boundRuleIds) {
+    if (!RuleIssues.class.equals(param.getType())) {
+      throw new IllegalArgumentException(
+          "can't use @ProvidedSonarScan on anything except RuleIssues (see "
+              + param.getDeclaringExecutable().getDeclaringClass().getName()
+              + ")");
+    }
+
+    final String ruleId = annotation.ruleId();
+
+    // Check if the ruleId has already been bound
+    if (boundRuleIds.containsKey(ruleId)) {
+      return; // Skip binding if already bound
+    }
+
+    // bind from existing scan
+    List<Issue> issues = issuesByRuleMap.get(annotation.ruleId());
+
+    if (issues == null || issues.isEmpty()) {
+      bind(RuleIssues.class).annotatedWith(annotation).toInstance(EMPTY);
+    } else {
+      RuleIssues ruleIssues = createRuleIssues(issues);
+      bind(RuleIssues.class).annotatedWith(annotation).toInstance(ruleIssues);
+      // Mark the ruleId as bound
+      boundRuleIds.put(ruleId, true);
+    }
+  }
+
+  private RuleIssues createRuleIssues(List<Issue> issues) {
+    Map<String, List<Issue>> issuesByPath = new HashMap<>();
+    issues.forEach(
+        issue -> {
+          Optional<String> filename = issue.componentFileName();
+          if (filename.isPresent()) {
+            String fullPath = repository.resolve(filename.get()).toString();
+            List<Issue> pathIssues = issuesByPath.computeIfAbsent(fullPath, f -> new ArrayList<>());
+            pathIssues.add(issue);
+          }
+        });
+    return new DefaultRuleIssues(issuesByPath);
   }
 
   private static final RuleIssues EMPTY = new DefaultRuleIssues(Map.of());
