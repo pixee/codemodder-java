d110f54e385e1f51b6d0991531a1f5880509401b
Carlos Uscanga <carlos.07um@gmail.com>
2024-06-13 15:54:37 -0600

    javadoc

--- plugins/codemodder-plugin-sonar/src/main/java/io/codemodder/providers/sonar/RuleHotspot.java
+++ plugins/codemodder-plugin-sonar/src/main/java/io/codemodder/providers/sonar/RuleHotspot.java
@@ -4,8 +4,9 @@ import io.codemodder.sonar.model.Hotspot;
 import java.nio.file.Path;
 import java.util.List;
 
-public class RuleHotspot extends DefaultRuleFinding<Hotspot> {
-  RuleHotspot(List<Hotspot> sonarFindings, final Path repository) {
-    super(sonarFindings, repository);
+/** Class type to bind {@link Hotspot} from {@link ProvidedSonarScan} */
+public final class RuleHotspot extends DefaultRuleFinding<Hotspot> {
+  RuleHotspot(List<Hotspot> hotspots, final Path repository) {
+    super(hotspots, repository);
   }
 }
--- plugins/codemodder-plugin-sonar/src/main/java/io/codemodder/providers/sonar/RuleIssue.java
+++ plugins/codemodder-plugin-sonar/src/main/java/io/codemodder/providers/sonar/RuleIssue.java
@@ -4,8 +4,9 @@ import io.codemodder.sonar.model.Issue;
 import java.nio.file.Path;
 import java.util.List;
 
-public class RuleIssue extends DefaultRuleFinding<Issue> {
-  RuleIssue(List<Issue> sonarFindings, final Path repository) {
-    super(sonarFindings, repository);
+/** Class type to bind {@link Issue} from {@link ProvidedSonarScan} */
+public final class RuleIssue extends DefaultRuleFinding<Issue> {
+  RuleIssue(List<Issue> issues, final Path repository) {
+    super(issues, repository);
   }
 }
