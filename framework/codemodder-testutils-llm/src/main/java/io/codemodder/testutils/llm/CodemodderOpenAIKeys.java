903df5960436558e5bdb6777c5bef55b736b8d22
Johnathan Gilday <johnathan.gilday@pixee.ai>
2024-06-24 11:31:01 -0400

    ✅ Add OpenAI Integration Tests to GitHub Actions Workflow

--- framework/codemodder-testutils-llm/src/main/java/io/codemodder/testutils/llm/CodemodderOpenAIKeys.java
+++ framework/codemodder-testutils-llm/src/main/java/io/codemodder/testutils/llm/CodemodderOpenAIKeys.java
@@ -12,9 +12,17 @@ public final class CodemodderOpenAIKeys {
   /**
    * Return true if and only if we have a non-empty {@code CODEMODDER_OPENAI_API_KEY} environment
    * variable.
+   *
+   * @throws IllegalStateException when running in a CI/CD environment and the key is not available.
+   *     This is backstop to ensure that all expected tests run in CI/CD.
    */
   public static boolean isAvailable() {
     String key = System.getenv("CODEMODDER_OPENAI_API_KEY");
-    return key != null && !key.isBlank();
+    final var available = key != null && !key.isBlank();
+    if (System.getenv("CI") != null && !available) {
+      throw new IllegalStateException(
+          "The CODEMODDER_OPENAI_API_KEY environment variable must be set in the CI/CD environment.");
+    }
+    return available;
   }
 }
