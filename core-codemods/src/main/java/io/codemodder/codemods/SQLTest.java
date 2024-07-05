c429750912ecdad7ac905a82d91919df9407deae
andrecs <12188364+andrecsilva@users.noreply.github.com>
2024-07-05 09:03:46 -0300

    remove me

---
+++ core-codemods/src/main/java/io/codemodder/codemods/SQLTest.java
@@ -0,0 +1,20 @@
+package com.acme.testcode;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public final class Test {
+
+  private Connection conn;
+
+  public ResultSet simpleIndirect() throws SQLException {
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    String sql = "SELECT * FROM " + input;
+    Statement stmt = conn.createStatement();
+    return stmt.executeQuery(sql);
+  }
+}
