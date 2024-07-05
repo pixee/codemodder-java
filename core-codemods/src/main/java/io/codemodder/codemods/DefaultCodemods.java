1588c132ea927e49b1beae731ed19608a8c8be91
andrecs <12188364+andrecsilva@users.noreply.github.com>
2024-07-05 13:54:01 -0300

    Removed find and fix codemod from defaults

--- core-codemods/src/main/java/io/codemodder/codemods/DefaultCodemods.java
+++ core-codemods/src/main/java/io/codemodder/codemods/DefaultCodemods.java
@@ -58,7 +58,6 @@ public final class DefaultCodemods {
         SubstituteReplaceAllCodemod.class,
         SonarXXECodemod.class,
         SQLParameterizerCodemod.class,
-        SQLTableInjectionFilterCodemod.class,
         SSRFCodemod.class,
         StackTraceExposureCodemod.class,
         SwitchLiteralFirstComparisonsCodemod.class,
