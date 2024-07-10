c58ccf32d33ddc96d8dd5d622dc1e5412eef9c82
Dan D'Avella <dan.davella@pixee.ai>
2024-07-10 13:25:56 -0400

    Add gpt-4-turbo model; use for code changing (#416)
    Without function calling, we need to use models that support
    `json_object` as a response format.
    
    At some point we should revisit function calling with the new client,
    but it seems substantially more complicated than the old API. It also
    appears to be deprecated in favor of "tool calling"":
    
    > For function call sample, see [function
    call](https://github.com/Azure/azure-sdk-for-java/tree/azure-ai-openai_1.0.0-beta.10/sdk/openai/azure-ai-openai/src/samples/java/com/azure/ai/openai/ChatCompletionsFunctionCall.java).
    However, they are considered a legacy feature. Using tools is the
    preferred way. For more details see sample [tool
    calls](https://github.com/Azure/azure-sdk-for-java/blob/azure-ai-openai_1.0.0-beta.10/sdk/openai/azure-ai-openai/src/samples/java/com/azure/ai/openai/usage/GetChatCompletionsToolCallSample.java).
    
    
    https://learn.microsoft.com/en-us/java/api/overview/azure/ai-openai-readme?view=azure-java-preview#chat-completions

--- plugins/codemodder-plugin-llm/src/main/java/io/codemodder/plugins/llm/StandardModel.java
+++ plugins/codemodder-plugin-llm/src/main/java/io/codemodder/plugins/llm/StandardModel.java
@@ -17,6 +17,12 @@ public enum StandardModel implements Model {
       return Tokens.countTokens(messages, 3, EncodingType.CL100K_BASE);
     }
   },
+  GPT_4_TURBO_2024_04_09("gpt-4-turbo-2024-04-09", 128_000) {
+    @Override
+    public int tokens(final List<String> messages) {
+      return Tokens.countTokens(messages, 3, EncodingType.CL100K_BASE);
+    }
+  },
   GPT_4O_2024_05_13("gpt-4o-2024-05-13", 128_000) {
     /**
      * This is wrong - we copy / pasted from GPT 3.5 while we await GPT-4o token counting support <a
