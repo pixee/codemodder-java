cfb7b01cde83f09f746acfff9dca7d429a3874ba
Daniel D'Avella <dan.davella@pixee.ai>
2024-07-11 15:16:10 -0400

    Address code review comments

--- core-codemods/src/main/java/io/codemodder/codemods/DefaultCodemods.java
+++ core-codemods/src/main/java/io/codemodder/codemods/DefaultCodemods.java
@@ -35,7 +35,6 @@ public final class DefaultCodemods {
         JDBCResourceLeakCodemod.class,
         JEXLInjectionCodemod.class,
         JSPScriptletXSSCodemod.class,
-        // LogFailedLoginCodemod.class,
         LimitReadlineCodemod.class,
         MavenSecureURLCodemod.class,
         OutputResourceLeakCodemod.class,
--- plugins/codemodder-plugin-llm/src/main/java/io/codemodder/plugins/llm/SarifPluginLLMCodemod.java
+++ plugins/codemodder-plugin-llm/src/main/java/io/codemodder/plugins/llm/SarifPluginLLMCodemod.java
@@ -4,6 +4,7 @@ import io.codemodder.RuleSarif;
 import io.codemodder.SarifPluginRawFileChanger;
 import java.util.Objects;
 
+/** A base class for LLM codemods that process SARIF and use the OpenAI service. */
 public abstract class SarifPluginLLMCodemod extends SarifPluginRawFileChanger {
   protected final OpenAIService openAI;
 
@@ -12,6 +13,12 @@ public abstract class SarifPluginLLMCodemod extends SarifPluginRawFileChanger {
     this.openAI = Objects.requireNonNull(openAI);
   }
 
+  /**
+   * Indicates whether the codemod should run.
+   *
+   * <p>Subclasses can override this method to add additional hecks but should call
+   * super.shouldRun() to ensure the OpenAI service is available.
+   */
   @Override
   public boolean shouldRun() {
     return openAI.isServiceAvailable();
