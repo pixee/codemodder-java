fd9b8a29cda3b22a6cc2b99025d12b1d1b2e298f
Johnathan Gilday <johnathan.gilday@pixee.ai>
2024-07-18 12:28:31 -0400

    ✅ Added Tests for EnvironmentBasedModelMapper
    While working to confirm that the logic in codemodder-java and the
    platform is in sync, I found opportunities to add missing tests and fix
    some pitfalls.
    
    /towards #work

--- plugins/codemodder-plugin-llm/src/main/java/io/codemodder/plugins/llm/EnvironmentBasedModelMapper.java
+++ plugins/codemodder-plugin-llm/src/main/java/io/codemodder/plugins/llm/EnvironmentBasedModelMapper.java
@@ -1,22 +1,34 @@
 package io.codemodder.plugins.llm;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /** Mapper that maps models to their deployment names based on environment variables. */
 final class EnvironmentBasedModelMapper implements ModelMapper {
-  private static final String DEPLOYMENT_TEMPLATE = "CODEMODDER_AZURE_OPENAI_%s_DEPLOYMENT";
 
   private final HashMap<Model, String> map = new HashMap<>();
 
   EnvironmentBasedModelMapper() {
-    for (Model m : StandardModel.values()) {
-      final var deployment = System.getenv(String.format(DEPLOYMENT_TEMPLATE, m));
-      map.put(m, deployment == null ? m.id() : deployment);
+    this(System.getenv());
+  }
+
+  EnvironmentBasedModelMapper(final Map<String, String> environment) {
+    for (final Model model : StandardModel.values()) {
+      final var name = String.format(DEPLOYMENT_TEMPLATE, toEnvironmentVariableCase(model.id()));
+      final var deployment = environment.getOrDefault(name, model.id());
+      map.put(model, deployment);
     }
   }
 
   @Override
   public String getModelName(Model model) {
-    return map.get(model);
+    return map.getOrDefault(model, model.id());
   }
+
+  /** Converts a model ID to environment variable casing. */
+  private static String toEnvironmentVariableCase(String input) {
+    return input.toUpperCase().replace('-', '_').replace('.', '_');
+  }
+
+  private static final String DEPLOYMENT_TEMPLATE = "CODEMODDER_AZURE_OPENAI_%s_DEPLOYMENT";
 }
