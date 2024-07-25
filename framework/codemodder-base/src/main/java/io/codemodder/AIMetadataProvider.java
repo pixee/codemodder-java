18c21470151e848525484c9816ba8c9a8da2c8b0
Ryan Dens <ryan.dens@pixee.ai>
2024-07-25 12:00:49 -0700

    :recycle: make interface package private for now

--- framework/codemodder-base/src/main/java/io/codemodder/AIMetadataProvider.java
+++ framework/codemodder-base/src/main/java/io/codemodder/AIMetadataProvider.java
@@ -3,7 +3,7 @@ package io.codemodder;
 import io.codemodder.codetf.CodeTFAiMetadata;
 
 /** Marks a class so that it can provide {@link CodeTFAiMetadata} to its consumers. */
-public interface AIMetadataProvider {
+interface AIMetadataProvider {
 
   /**
    * @return the {@link CodeTFAiMetadata} describing how a class used an AI service.
