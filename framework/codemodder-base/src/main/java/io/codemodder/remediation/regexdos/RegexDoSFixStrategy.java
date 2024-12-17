b8b90cd92de17020e8d849d4f5e581edfe0e93a7
andrecs <12188364+andrecsilva@users.noreply.github.com>
2024-12-17 09:43:40 -0300

    Modified transformation to use security package

--- framework/codemodder-base/src/main/java/io/codemodder/remediation/regexdos/RegexDoSFixStrategy.java
+++ framework/codemodder-base/src/main/java/io/codemodder/remediation/regexdos/RegexDoSFixStrategy.java
@@ -1,15 +1,10 @@
 package io.codemodder.remediation.regexdos;
 
-import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.expr.Expression;
-import com.github.javaparser.ast.expr.IntegerLiteralExpr;
-import com.github.javaparser.ast.expr.LambdaExpr;
-import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.*;
+import io.codemodder.DependencyGAV;
 import io.codemodder.ast.ASTTransforms;
 import io.codemodder.ast.ASTs;
 import io.codemodder.ast.LocalDeclaration;
@@ -18,8 +13,6 @@ import io.codemodder.remediation.MatchAndFixStrategy;
 import io.codemodder.remediation.SuccessOrReason;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
 
 /** Adds a timeout function and wraps regex match call with it * */
 final class RegexDoSFixStrategy extends MatchAndFixStrategy {
@@ -51,33 +44,6 @@ final class RegexDoSFixStrategy extends MatchAndFixStrategy {
         .isPresent();
   }
 
-  private static void addTimeoutMethodIfMissing(
-      final CompilationUnit cu, final ClassOrInterfaceDeclaration classDecl) {
-    final String method =
-        """
-                public <E> E executeWithTimeout(final Callable<E> action, final int timeout){
-                  Future<E> maybeResult = Executors.newSingleThreadExecutor().submit(action);
-                  try{
-                      return maybeResult.get(timeout, TimeUnit.MILLISECONDS);
-                  }catch(Exception e){
-                      throw new RuntimeException("Failed to execute within time limit.");
-                  }
-                }
-            """;
-    boolean filterMethodPresent =
-        classDecl.findAll(MethodDeclaration.class).stream()
-            .anyMatch(
-                md ->
-                    md.getNameAsString().equals("executeWithTimeout")
-                        && md.getParameters().size() == 2);
-    if (!filterMethodPresent) {
-      classDecl.addMember(StaticJavaParser.parseMethodDeclaration(method));
-    }
-    // Add needed import
-    ASTTransforms.addImportIfMissing(cu, Callable.class.getName());
-    ASTTransforms.addImportIfMissing(cu, Executors.class.getName());
-  }
-
   @Override
   public SuccessOrReason fix(final CompilationUnit cu, final Node node) {
     // indirect case, assigned to a variable
@@ -91,21 +57,20 @@ final class RegexDoSFixStrategy extends MatchAndFixStrategy {
     if (allValidMethodCalls.isEmpty()) {
       return SuccessOrReason.reason("Couldn't find any matching methods");
     }
-    // Add executeWithTimout method to the encompassing class
-    var classDecl = call.findAncestor(ClassOrInterfaceDeclaration.class);
-    if (classDecl.isEmpty()) {
-      return SuccessOrReason.reason("Couldn't find encompassing class");
-    }
-    classDecl.ifPresent(cd -> addTimeoutMethodIfMissing(cu, cd));
+
     for (var mce : allValidMethodCalls) {
       // Wrap it with executeWithTimeout with a default 5000 of timeout
       var newCall =
           new MethodCallExpr(
+              new NameExpr("ExecuteWithTimeout"),
               "executeWithTimeout",
-              new LambdaExpr(new NodeList<>(), mce.clone()),
-              new IntegerLiteralExpr(DEFAULT_TIMEOUT));
+              new NodeList<>(
+                  new LambdaExpr(new NodeList<>(), mce.clone()),
+                  new IntegerLiteralExpr(DEFAULT_TIMEOUT)));
       mce.replace(newCall);
     }
-    return SuccessOrReason.success();
+
+    ASTTransforms.addImportIfMissing(cu, "io.github.pixee.security.ExecuteWithTimeout");
+    return SuccessOrReason.success(List.of(DependencyGAV.JAVA_SECURITY_TOOLKIT));
   }
 }
